8ec15d4240c0ca94cb046ad76cdf956a59a5c7a4
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2019-06-14 13:34:45 -0400

    Store password securely with keyring

--- sardes/config/database.py
+++ sardes/config/database.py
@@ -16,8 +16,12 @@ import time
 # ---- Third party imports
 from appconfigs.base import get_config_dir
 from sardes import __appname__
+try:
+    import keyring
+except Exception:
+    keyring = None
 
-DB_CONNECTION_KEYS = ['dbname', 'user', 'host', 'password']
+DB_CONNECTION_KEYS = ['database', 'user', 'host']
 
 
 def get_dbconfig():
@@ -29,9 +33,11 @@ def get_dbconfig():
     if osp.exists(filename):
         with open(filename) as json_file:
             dbconfig = json.load(json_file)
-        return dbconfig
     else:
-        return {}
+        dbconfig = {key: '' for key in DB_CONNECTION_KEYS}
+    dbconfig['password'] = get_password(dbconfig['database'], dbconfig['user'])
+
+    return dbconfig
 
 
 def set_dbconfig(**kargs):
@@ -40,8 +46,14 @@ def set_dbconfig(**kargs):
     configuration file.
     """
     dbconfig = get_dbconfig()
-    dbconfig.update({key: value for key, value in kargs.items() if
-                     key in DB_CONNECTION_KEYS})
+    dbconfig.update(kargs)
+
+    store_password(
+        dbconfig['database'], dbconfig['user'], dbconfig['password'])
+
+    # Remove keys that do not belong in our configs.
+    dbconfig = {key: value for key, value in dbconfig.items() if
+                key in DB_CONNECTION_KEYS}
 
     filename = osp.join(get_config_dir(__appname__), 'dbconfig.json')
     try:
@@ -59,3 +71,28 @@ def set_dbconfig(**kargs):
             print("Failed to write database configuration file to disk, with "
                   "the exception shown below.")
             print(e)
+
+
+def store_password(database, username, password):
+    """Store credentials securely for future use if possible."""
+    if keyring is not None and database and username and password:
+        try:
+            keyring.set_password(
+                __appname__, "{}/{}".format(database, username), password)
+        except Exception as e:
+            print(e)
+        else:
+            print('Password saved successfully.')
+
+
+def get_password(database, username):
+    """
+    Get pasword saved for that database and username or else return an empty
+    string.
+    """
+    if keyring is not None and database and username:
+        password = keyring.get_password(
+            __appname__, "{}/{}".format(database, username))
+        return password or ''
+    else:
+        return ''
