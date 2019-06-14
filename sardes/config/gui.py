1c83ad68915e6a3545c685319f6fe8e09c520ab2
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2019-06-14 11:10:37 -0400

    Add color definition to config/gui.py

---
+++ sardes/config/gui.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# Copyright © SARDES Project Contributors
+# https://github.com/cgq-qgc/sardes
+#
+# This file is part of SARDES.
+# Licensed under the terms of the GNU General Public License.
+# ----------------------------------------------------------------------------
+
+ICON_COLOR = '#4d4d4d'
+GREEN = '#00aa00'
+RED = '#CC0000'
