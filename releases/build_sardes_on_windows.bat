68132e081fc39779e1656831872ef44badd66cad
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2019-06-16 15:41:53 -0400

    Add the .bat file to build package locally

---
+++ releases/build_sardes_on_windows.bat
@@ -0,0 +1,5 @@
+C:\Python37\python.exe --upgrade pip
+C:\Python37\python.exe -r ..\requirements.txt
+C:\Python37\python.exe -m pip install pyinstaller pywin32 tornado
+C:\Python37\Scripts\pyinstaller.exe sardes.spec
+pause
