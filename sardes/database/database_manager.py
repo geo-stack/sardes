2eb395cb4067aff1591c351c981281075956a134
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2019-07-26 10:07:51 -0400

    Streamline worker communication pipeline

--- sardes/database/database_manager.py
+++ sardes/database/database_manager.py
@@ -9,7 +9,8 @@
 
 
 # ---- Standard imports
-import sys
+import uuid
+from collections import OrderedDict
 
 # ---- Third party imports
 from pandas import DataFrame
@@ -20,77 +21,71 @@ class DatabaseConnectionWorker(QObject):
     """
     A simple worker to create a new database session without blocking the gui.
     """
-    sig_database_connected = Signal(object, object)
-    sig_database_disconnected = Signal()
-    sig_database_observation_wells = Signal(DataFrame)
+    sig_task_completed = Signal(object, object)
 
     def __init__(self, parent=None):
         super(DatabaseConnectionWorker, self).__init__(parent)
         self.db_accessor = None
+        self._tasks = OrderedDict()
 
-        self._tasks = []
-
-    def add_task(self, task, *args, **kargs):
+    # ---- Task management
+    def add_task(self, task_uuid4, task, *args, **kargs):
         """
         Add a task to the stack that will be executed when the thread of
         this worker is started.
         """
-        self._tasks.append((task, args, kargs))
+        self._tasks[task_uuid4] = (task, args, kargs)
 
     def run_tasks(self):
         """Execute the tasks that were added to the stack."""
-        for task, args, kargs in self._tasks:
-            method_to_exec = getattr(self, task)
-            method_to_exec(*args, **kargs)
-        self._tasks = []
+        for task_uuid4, (task, args, kargs) in self._tasks.items():
+            method_to_exec = getattr(self, '_' + task)
+            returned_values = method_to_exec(*args, **kargs)
+            self.sig_task_completed.emit(task_uuid4, returned_values)
+        self._tasks = OrderedDict()
         self.thread().quit()
 
+    # ---- Worker connection state
     def is_connected(self):
         """Return whether a connection to a database is currently active."""
         return self.db_accessor is not None and self.db_accessor.is_connected()
 
-    def connect_to_db(self, db_accessor):
+    # ---- Task definition
+    def _connect_to_db(self, db_accessor):
         """Try to create a new connection with the database"""
         self.db_accessor = db_accessor
         self.db_accessor.connect()
-        self.sig_database_connected.emit(
-            self.db_accessor._connection, self.db_accessor._connection_error)
+        return self.db_accessor._connection, self.db_accessor._connection_error
 
-    def disconnect_from_db(self):
+    def _disconnect_from_db(self):
         """Close the connection with the database"""
         if self.db_accessor is not None:
             self.db_accessor.close_connection()
-        self.sig_database_disconnected.emit()
+        return None,
 
-    def get_observation_wells(self):
+    def _get_observation_wells(self):
         """
         Try get the list of observation wells that are saved in the database
-        and send the results through the sig_database_observation_wells
+        and send the results through the sig_observation_wells_fetched
         signal.
         """
         try:
-            locations = self.db_accessor.get_observation_wells()
+            obs_wells = self.db_accessor.get_observation_wells()
         except AttributeError:
-            locations = DataFrame([])
-        self.sig_database_observation_wells.emit(locations)
+            obs_wells = DataFrame([])
+        return obs_wells,
 
-    def execute_sql_request(self, sql_request, **kwargs):
-        """Execute a SQL statement construct and return a ResultProxy."""
-        if self.db_accessor is not None:
-            return self.db_accessor.execute(sql_request, **kwargs)
 
 
 class DatabaseConnectionManager(QObject):
     sig_database_connected = Signal(object, object)
     sig_database_disconnected = Signal()
     sig_database_connection_changed = Signal(bool)
-    sig_database_observation_wells = Signal(DataFrame)
-    sig_new_database_accessor_registered = Signal()
 
     def __init__(self, parent=None):
         super().__init__(parent)
 
-        self._db_accessors = {}
+        self._task_callbacks = {}
 
         self._db_connection_worker = DatabaseConnectionWorker()
         self._db_connection_thread = QThread()
@@ -99,22 +94,10 @@ class DatabaseConnectionManager(QObject):
             self._db_connection_worker.run_tasks)
 
         # Connect the worker signals to handlers.
-        self._db_connection_worker.sig_database_connected.connect(
-            self._handle_connect_to_db)
-        self._db_connection_worker.sig_database_disconnected.connect(
-            self._handle_disconnect_from_db)
-        self._db_connection_worker.sig_database_observation_wells.connect(
-            self._handle_get_observation_wells)
-
-        self._db_connection_worker.sig_database_connected.connect(
-            lambda: self.sig_database_connection_changed.emit(
-                self.is_connected()))
-        self._db_connection_worker.sig_database_disconnected.connect(
-            lambda: self.sig_database_connection_changed.emit(
-                self.is_connected()))
+        self._db_connection_worker.sig_task_completed.connect(
+            self._exec_callback)
 
         self._is_connecting = False
-        self._locations = []
 
     def is_connected(self):
         """Return whether a connection to a database is currently active."""
@@ -126,6 +109,33 @@ class DatabaseConnectionManager(QObject):
         """
         return self._is_connecting
 
+    # ---- Public methods
+    def connect_to_db(self, db_accessor):
+        """
+        Try to create a new connection with the database using the
+        provided database accessor.
+        """
+        self._is_connecting = True
+        self._add_task(
+            'connect_to_db', self._handle_connect_to_db, db_accessor)
+        self._db_connection_thread.start()
+
+    def disconnect_from_db(self):
+        """Close the connection with the database"""
+        self._add_task('disconnect_from_db', self._handle_disconnect_from_db)
+        self._db_connection_thread.start()
+
+    def get_observation_wells(self, callback):
+        """
+        Get the list of observation wells that are saved in the database.
+
+        The results are sent through the sig_database_observation_wells signal
+        as a list of ObservationWell objects.
+        """
+        self._add_task('get_observation_wells', callback)
+        self._db_connection_thread.start()
+
+    # ---- Handlers
     @Slot(object, object)
     def _handle_connect_to_db(self, connection, connection_error):
         """
@@ -134,43 +144,23 @@ class DatabaseConnectionManager(QObject):
         """
         self._is_connecting = False
         self.sig_database_connected.emit(connection, connection_error)
-
-    def connect_to_db(self, db_accessor):
-        """
-        Try to create a new connection with the database using the
-        provided database accessor.
-        """
-        self._is_connecting = True
-        self._db_connection_worker.add_task('connect_to_db', db_accessor)
-        self._db_connection_thread.start()
+        self.sig_database_connection_changed.emit(self.is_connected())
 
     @Slot()
-    def _handle_disconnect_from_db(self):
+    def _handle_disconnect_from_db(self, *args, **kargs):
         """
         Handle when the connection to the database was closed successfully.
         """
         self.sig_database_disconnected.emit()
+        self.sig_database_connection_changed.emit(self.is_connected())
 
-    def disconnect_from_db(self):
-        """Close the connection with the database"""
-        self._db_connection_worker.add_task('disconnect_from_db')
-        self._db_connection_thread.start()
+    @Slot(object, object)
+    def _exec_callback(self, task_uuid4, returned_values):
+        if self._task_callbacks[task_uuid4] is not None:
+            self._task_callbacks[task_uuid4](*returned_values)
+        del self._task_callbacks[task_uuid4]
 
-    @Slot(DataFrame)
-    def _handle_get_observation_wells(self, locations):
-        """
-        Handle when the list of observation wells saved in the database
-        was fetched successfully.
-        """
-        self._locations = locations
-        self.sig_database_observation_wells.emit(self._locations)
-
-    def get_observation_wells(self):
-        """
-        Get the list of observation wells that are saved in the database.
-
-        The results are sent through the sig_database_observation_wells signal
-        as a list of ObservationWell objects.
-        """
-        self._db_connection_worker.add_task('get_observation_wells')
-        self._db_connection_thread.start()
+    def _add_task(self, task, callback, *args, **kargs):
+        task_uuid4 = uuid.uuid4()
+        self._task_callbacks[task_uuid4] = callback
+        self._db_connection_worker.add_task(task_uuid4, task, *args, **kargs)
