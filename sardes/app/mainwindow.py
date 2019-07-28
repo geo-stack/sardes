89329fb3de533250f217c9f0c30bab2c86091b0e
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2019-07-28 19:32:49 -0400

    Load installed third party plugins directly
    There is no need to search for third party plugin because we do not want
    to support distributed plugin installation.

--- sardes/app/mainwindow.py
+++ sardes/app/mainwindow.py
@@ -78,6 +78,7 @@ class MainWindow(QMainWindow):
         self.create_topright_corner_toolbar()
         self.set_window_settings(*get_window_settings())
         self.setup_internal_plugins()
+        self.setup_thirdparty_plugins()
         # Note: The window state must be restored after the setup of this
         #       mainwindow plugins and toolbars.
         self._restore_window_state()
@@ -96,23 +97,31 @@ class MainWindow(QMainWindow):
         plugin.register_plugin()
         self.internal_plugins.append(plugin)
 
+    def setup_thirdparty_plugins(self):
+        """Setup Sardes third party plugins."""
+        installed_thirdparty_plugins = []
         self.thirdparty_plugins = []
+
         user_plugin_path = osp.join(CONFIG_DIR, 'plugins')
         if not osp.isdir(user_plugin_path):
             os.makedirs(user_plugin_path)
-        module_loaders = get_sardes_plugin_module_loaders(user_plugin_path)
-        for module_name, module_loader in module_loaders.items():
-            if (module_name in installed_user_plugins and
-                    module_name not in sys.modules):
+
+        for module_name in installed_thirdparty_plugins:
+            if module_name not in sys.modules:
                 try:
-                    module = module_loader.load_module()
-                    sys.modules[module_name] = module
-                    plugin = module.SARDES_PLUGIN_CLASS(self)
-                    plugin.register_plugin()
+                    module_spec = importlib.machinery.PathFinder.find_spec(
+                        module_name, [user_plugin_path])
+                    if module_spec:
+                        module = module_spec.loader.load_module()
+                        sys.modules[module_name] = module
+                        plugin = module.SARDES_PLUGIN_CLASS(self)
+                        plugin.register_plugin()
+                        self.thirdparty_plugins.append(plugin)
                 except Exception as error:
-                    print("%s: %s" % (module, str(error)))
-                else:
-                    self.thirdparty_plugins.append(plugin)
+                    print("{}: {}".format(module_name, str(error)))
+            else:
+                raise Warning(
+                    "{}: This module is already loaded.".format(module_name))
 
     # ---- Toolbar setup
     def create_topright_corner_toolbar(self):
