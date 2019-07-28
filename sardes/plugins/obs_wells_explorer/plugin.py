31940acfc40946c50a9b02d764b7b7fa6897c260
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2019-07-28 16:25:31 -0400

    Create a plugin Observation Wells plugin

---
+++ sardes/plugins/obs_wells_explorer/plugin.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# Copyright © SARDES Project Contributors
+# https://github.com/cgq-qgc/sardes
+#
+# This file is part of SARDES.
+# Licensed under the terms of the GNU General Public License.
+# -----------------------------------------------------------------------------
+
+# ---- Local imports
+from sardes.api.sardes_plugins import SardesPlugin, SardesPaneWidget
+from sardes.config.locale import _
+from sardes.widgets.locationtable import ObservationWellTableView
+
+"""Observation well explorer plugin"""
+
+
+class ObsWellsExplorer(SardesPlugin):
+
+    def __init__(self, parent):
+        super().__init__(parent)
+
+    def get_plugin_title(self):
+        """Return widget title"""
+        return _('Observation Wells')
+
+    def get_pane_widget(self):
+        main_widget = ObservationWellTableView(self.main.db_connection_manager)
+        pane_widget = SardesPaneWidget(parent=self.main)
+        pane_widget.set_main_widget(main_widget)
+        return pane_widget
