3b54646a59399ce09259db4f5d425db2dcf7f6e6
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2019-07-28 19:27:24 -0400

    Update pip in built bat file

--- releases/build_sardes_on_windows.bat
+++ releases/build_sardes_on_windows.bat
@@ -1,4 +1,4 @@
-C:\Python37\python.exe --upgrade pip
+C:\Python37\python.exe -m pip install --upgrade pip
 C:\Python37\python.exe -m pip install -r ..\requirements.txt
 C:\Python37\python.exe -m pip install pyinstaller pywin32 tornado
 C:\Python37\Scripts\pyinstaller.exe sardes.spec
