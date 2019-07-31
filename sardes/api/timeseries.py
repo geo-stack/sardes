da7a08b7dfbfdc010d658c523bc98a8aaef8ccf0
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2019-07-31 10:37:17 -0400

    Init implementation of TimeSeries class

---
+++ sardes/api/timeseries.py
@@ -0,0 +1,130 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# Copyright © SARDES Project Contributors
+# https://github.com/cgq-qgc/sardes
+#
+# This file is part of SARDES.
+# Licensed under the terms of the GNU General Public License.
+# -----------------------------------------------------------------------------
+
+# ---- Standard library imports
+from collections.abc import Mapping
+
+# ---- Third party imports
+from pandas import DatetimeIndex
+import numpy as np
+
+# ---- Local imports
+
+
+class TimeSeries(Mapping):
+    """
+    Sardes time series class.
+    """
+
+    def __init__(self, data, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._undo_stack = []
+        self._data = data
+        self._selected_data_indexes = DatetimeIndex([])
+
+    def __len__(self, key):
+        return len(self._data)
+
+    def __setitem__(self, key, value):
+        return NotImplementedError
+
+    def __getitem__(self, key):
+        return NotImplementedError
+
+    def __iter__(self):
+        return NotImplementedError
+
+    def __str__(self):
+        return self._data.__str__()
+
+    # ---- Attributes
+    @property
+    def data(self):
+        return self._data
+
+    @property
+    def dates(self):
+        return self.data.index.values
+
+    @property
+    def strftime(self):
+        return self.data.index.strftime("%Y-%m-%dT%H:%M:%S").values.tolist()
+
+    # ---- Data Selection
+    def select_data(self, xrange=None, yrange=None):
+        colname = self._data.columns[0]
+        if xrange and yrange:
+            indexes = (
+                self._data[(self._data.index >= xrange[0]) &
+                           (self._data.index <= xrange[1]) &
+                           (self._data[colname] >= yrange[0]) &
+                           (self._data[colname] <= yrange[1])
+                           ]).index
+        elif xrange:
+            indexes = (
+                self._data[(self._data.index >= xrange[0]) &
+                           (self._data.index <= xrange[1])
+                           ]).index
+        elif yrange:
+            indexes = (
+                self._data[(self._data[colname] >= yrange[0]) &
+                           (self._data[colname] <= yrange[1])
+                           ]).index
+        else:
+            indexes = DatetimeIndex([])
+
+        self._selected_data_indexes = (
+            self._selected_data_indexes.append(indexes))
+
+    def get_selected_data(self):
+        return self._data.loc[self._selected_data_indexes]
+
+    def clear_selected_data(self):
+        self._selected_data_indexes = DatetimeIndex([])
+
+    # ---- Versionning
+    @property
+    def has_uncommited_changes(self):
+        """"
+        Return whether there is uncommited changes to the water level data.
+        """
+        return bool(len(self._undo_stack))
+
+    def commit(self):
+        """Commit the changes made to the water level data to the project."""
+        raise NotImplementedError
+
+    def undo(self):
+        """Undo the last changes made to the water level data."""
+        if self.has_uncommited_changes:
+            changes = self._undo_stack.pop(-1)
+            self._data[changes.index] = changes
+
+    def clear_all_changes(self):
+        """
+        Clear all changes that were made to the water level data since the
+        last commit.
+        """
+        while self.has_uncommited_changes:
+            self.undo()
+
+    def delete_waterlevels_at(self, indexes):
+        """Delete the water level data at the specified indexes."""
+        if len(indexes):
+            self._add_to_undo_stack(indexes)
+            self._data.iloc[indexes] = np.nan
+
+    def _add_to_undo_stack(self, indexes):
+        """
+        Store the old water level values at the specified indexes in a stack
+        before changing or deleting them. This allow to undo or cancel any
+        changes made to the water level data before commiting them.
+        """
+        if len(indexes):
+            self._undo_stack.append(self._data.iloc[indexes, 0].copy())
