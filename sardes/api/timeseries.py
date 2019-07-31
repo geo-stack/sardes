da075f6b6d08e363b88cae6a72a38238ac7050e3
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2019-07-31 14:55:44 -0400

    Update imports

--- sardes/api/timeseries.py
+++ sardes/api/timeseries.py
@@ -11,7 +11,7 @@
 from collections.abc import Mapping
 
 # ---- Third party imports
-from pandas import DatetimeIndex, Series
+from pandas import DatetimeIndex
 import numpy as np
 
 # ---- Local imports
