c84aebd66a10041c0a8cebb0bb616fcb57e22d9a
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2019-08-16 10:29:42 -0400

    Implement timeseries and obs api in demo accessor

--- sardes/database/accessor_demo.py
+++ sardes/database/accessor_demo.py
@@ -12,27 +12,40 @@ from time import sleep
 from copy import deepcopy
 
 # ---- Third party imports
+import numpy as np
 import pandas as pd
+from pandas import Series
 
 # ---- Local imports
 from sardes.api.database_accessor import DatabaseAccessorBase
+from sardes.api.timeseries import TimeSeries
 
-
-OBS_WELLS_DATA = {}
+OBS_WELLS_COLUMNS = ['obs_well_id', 'common_name', 'municipality',
+                     'aquifer_type', 'confinement', 'aquifer_code',
+                     'in_recharge_zone', 'is_influenced', 'latitude',
+                     'longitude', 'is_station_active', 'obs_well_notes']
+OBS_WELLS_DATA = []
 for i in range(5):
-    OBS_WELLS_DATA['obs_well_id'] = 'obs_well_id #{}'.format(i)
-    OBS_WELLS_DATA['common_name'] = 'common_name #{}'.format(i)
-    OBS_WELLS_DATA['municipality'] = 'municipality #{}'.format(i)
-    OBS_WELLS_DATA['aquifer_type'] = 'aquifer #{}'.format(i)
-    OBS_WELLS_DATA['confinement'] = 'confinement #{}'.format(i)
-    OBS_WELLS_DATA['aquifer_code'] = i
-    OBS_WELLS_DATA['in_recharge_zone'] = 'zone_rechar #{}'.format(i)
-    OBS_WELLS_DATA['is_influenced'] = 'influences #{}'.format(i)
-    OBS_WELLS_DATA['latitude'] = 45 + i / 10
-    OBS_WELLS_DATA['longitude'] = -75 + i / 10
-    OBS_WELLS_DATA['is_station_active'] = 'station_active #{}'.format(i)
-    OBS_WELLS_DATA['obs_well_notes'] = 'note #{}'.format(i)
-OBS_WELLS_DF = pd.DataFrame(OBS_WELLS_DATA, range(5))
+    OBS_WELLS_DATA.append([
+        'obs_well_id #{}'.format(i),
+        'common_name #{}'.format(i),
+        'municipality #{}'.format(i),
+        'aquifer #{}'.format(i),
+        'confinement #{}'.format(i),
+        i,
+        'zone_rechar #{}'.format(i),
+        'influences #{}'.format(i),
+        45 + i / 10,
+        -75 + i / 10,
+        'station_active #{}'.format(i),
+        'note #{}'.format(i)])
+OBS_WELLS_DF = pd.DataFrame(OBS_WELLS_DATA, columns=OBS_WELLS_COLUMNS)
+
+MONITORED_PROPERTIES = ['COND_ELEC', 'NIV_EAU', 'TEMP']
+MONITORED_PROPERTY_NAMES = {
+    'COND_ELEC': "Water electrical conductivity",
+    'NIV_EAU': "Water level",
+    'TEMP': "Water level temperature"}
 
 
 class DatabaseAccessorDemo(DatabaseAccessorBase):
@@ -74,7 +87,16 @@ class DatabaseAccessorDemo(DatabaseAccessorBase):
         self._connection = None
         print("done")
 
-    def get_observation_wells(self, sort=True):
+    # ---- Observation wells
+    @property
+    def observation_wells(self):
+        """
+        Return the list of observation wells that are saved in the
+        database.
+        """
+        return OBS_WELLS_DF['obs_well_id'].values
+
+    def get_observation_wells(self):
         """
         Get a list of ObservationWell objects containing information related
         to the observation wells that are saved in the database.
@@ -84,8 +106,78 @@ class DatabaseAccessorDemo(DatabaseAccessorBase):
         print("done")
         return self._wells if self.is_connected() else []
 
+    # ---- Monitored properties
+    @property
+    def monitored_properties(self):
+        """
+        Returns the list of properties for which time data is stored in the
+        database.
+        """
+        return MONITORED_PROPERTIES
+
+    def get_monitored_property_name(self, monitored_property):
+        """
+        Return the common human readable name for the corresponding
+        monitored property.
+        """
+        return MONITORED_PROPERTY_NAMES[monitored_property]
+
+    def get_monitored_property_units(self, monitored_property):
+        """
+        Return the units in which the time data for this monitored property
+        are saved in the database.
+        """
+        return "UNITS"
+
+    def get_monitored_property_color(self, monitored_property):
+        return {'NIV_EAU': 'blue',
+                'TEMP': 'red',
+                'COND_ELEC': 'cyan'
+                }[monitored_property]
+
+    def get_timeseries_for_obs_well(self, obs_well_id, monitored_property):
+        """
+        Return a list of :class:`TimeSeries` objects
+        containing the data acquired in the observation well for the
+        specified monitored property.
+        """
+        DATE_RANGE = pd.date_range(start='1/1/2015', end='1/1/2019')
+        NYEAR = DATE_RANGE[-1].year - DATE_RANGE[0].year + 1
+        YEARLY_RADS = np.arange(
+            0, 2 * NYEAR * np.pi, 2 * NYEAR * np.pi / len(DATE_RANGE))
+        if monitored_property == 'TEMP':
+            TSERIES_VALUES = 25 * np.sin(YEARLY_RADS) + 5
+            TSERIES_VALUES += 3 * np.random.rand(len(TSERIES_VALUES))
+        elif monitored_property == 'NIV_EAU':
+            TSERIES_VALUES = np.hstack((
+                np.linspace(100, 95, len(YEARLY_RADS) / 2),
+                np.linspace(95, 98, len(YEARLY_RADS) / 2)))
+            TSERIES_VALUES += 1 * np.sin(YEARLY_RADS)
+            TSERIES_VALUES += 2 * np.sin(YEARLY_RADS * 2)
+            TSERIES_VALUES += 1 * np.sin(YEARLY_RADS * 4)
+            TSERIES_VALUES += 0.5 * np.sin(YEARLY_RADS * 8)
+            TSERIES_VALUES += 0.25 * np.random.rand(len(TSERIES_VALUES))
+        else:
+            TSERIES_VALUES = []
+
+        tseries = TimeSeries(
+            Series(TSERIES_VALUES, index=DATE_RANGE),
+            tseries_id="CHANNEL_UUID",
+            tseries_name=(
+                self.get_monitored_property_name(monitored_property)),
+            tseries_units=(
+                self.get_monitored_property_units(monitored_property)),
+            tseries_color=(
+                self.get_monitored_property_color(monitored_property))
+            )
+        return [tseries]
+
 
 if __name__ == '__main__':
     accessor = DatabaseAccessorDemo()
     accessor.connect()
     obs_wells = accessor.get_observation_wells()
+    wlevel = accessor.get_timeseries_for_obs_well('dfsdf', 'NIV_EAU')
+    print(wlevel[0])
+    print(accessor.observation_wells)
+    print(accessor.monitored_properties)
