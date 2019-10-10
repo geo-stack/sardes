616d1b26f18e63f48fc91987405a4647d3044d56
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2019-10-10 08:52:48 -0400

    Define get_sondes_data in API

--- sardes/api/database_accessor.py
+++ sardes/api/database_accessor.py
@@ -154,6 +154,52 @@ class DatabaseAccessorBase(ABC):
         """
         return DataFrame([])
 
+    # ---- Sondes
+    def get_sondes_data(self):
+        """
+        Return a :class:`pandas.DataFrame` containing the information related
+        to the sondes used to monitor groundwater properties in the wells.
+
+        Returns
+        -------
+        :class:`pandas.DataFrame`
+            A :class:`pandas.DataFrame` containing the information related
+            to the sondes used to monitor groundwater properties in the wells.
+
+            The row indexes of the dataframe must correspond to the
+            sonde IDs, which are unique identifiers used to reference the
+            sondes in the database.
+
+            The dataframe can contain any of the columns that are
+            listed below.
+
+            Columns
+            ~~~~~~~~~~~~~~~~
+            - sonde_id: str
+                The unique identifier of the sonde.
+            - sonde_brand: str
+                The brand of the sonde.
+            - sonde_model: str
+                The model of the sonde.
+            - date_reception: datetime
+                The date when the sonde was added to the inventory.
+            - date_withdrawal: datetime
+                The date when the sonde was removed from the inventory.
+            - in_repair: bool
+                Indicate wheter the sonde is currently being repaired.
+            - out_of_order: bool
+                Indicate whether the sonde is out of order.
+                unconsolidated sediment.
+            - lost: bool
+                Indicates whether the sonde has been lost.
+            - off_network: bool
+                Indicate whether the sonde is currently being used outside
+                of the monitoring network.
+            - sonde_notes: str
+                Any notes related to the sonde.
+        """
+        return DataFrame([])
+
     # ---- Monitored properties
     @property
     @abstractmethod
