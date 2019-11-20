1b905c67d80cace4f0a1b63b8ef676139184dc2a
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2019-11-20 13:48:35 -0500

    ProcessStatusBar: add a vertical option

--- sardes/widgets/statusbar.py
+++ sardes/widgets/statusbar.py
@@ -24,12 +24,15 @@ class ProcessStatusBar(QWidget):
     PROCESS_SUCCEEDED = 2
     PROCESS_FAILED = 3
 
-    def __init__(self, parent=None):
+    def __init__(self, parent=None, orientation=Qt.Horizontal):
         super().__init__(parent)
         self._status = self.HIDDEN
 
         self._label = QLabel()
-        self._label.setAlignment(Qt.AlignLeft | Qt.AlignVCenter)
+        if orientation == Qt.Horizontal:
+            self._label.setAlignment(Qt.AlignLeft | Qt.AlignVCenter)
+        else:
+            self._label.setAlignment(Qt.AlignCenter | Qt.AlignVCenter)
         self._label.setWordWrap(True)
         self._label.setTextInteractionFlags(Qt.TextSelectableByMouse)
 
@@ -44,17 +47,26 @@ class ProcessStatusBar(QWidget):
         self._success_icon.hide()
 
         layout = QGridLayout(self)
+        layout.setContentsMargins(0, 0, 0, 0)
 
         alignment = Qt.AlignLeft | Qt.AlignVCenter
-        layout.addWidget(self._spinner, 1, 0, alignment)
-        layout.addWidget(self._failed_icon, 1, 0, alignment)
-        layout.addWidget(self._success_icon, 1, 0, alignment)
-        layout.setColumnMinimumWidth(1, 5)
-        layout.addWidget(self._label, 1, 2)
-
-        layout.setRowStretch(0, 100)
-        layout.setRowStretch(3, 100)
-        layout.setColumnStretch(2, 100)
+        layout.addWidget(self._spinner, 1, 1, alignment)
+        layout.addWidget(self._failed_icon, 1, 1, alignment)
+        layout.addWidget(self._success_icon, 1, 1, alignment)
+        if orientation == Qt.Horizontal:
+            layout.setColumnMinimumWidth(2, 5)
+            layout.addWidget(self._label, 1, 3)
+            layout.setRowStretch(0, 100)
+            layout.setRowStretch(3, 100)
+            layout.setColumnStretch(3, 100)
+            layout.setSpacing(0)
+        else:
+            layout.addWidget(self._label, 2, 1)
+            layout.setRowStretch(0, 100)
+            layout.setRowStretch(4, 100)
+            layout.setColumnStretch(0, 100)
+            layout.setColumnStretch(2, 100)
+            layout.setSpacing(5)
 
     @property
     def status(self):
