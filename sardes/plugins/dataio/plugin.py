a0c64828af32275af30a1854ce7269f632d7d9cb
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2020-02-10 09:39:36 -0500

    Create dataio plugin class

--- sardes/plugins/dataio/plugin.py
+++ sardes/plugins/dataio/plugin.py
@@ -33,6 +33,71 @@ from sardes.widgets.tableviews import NotEditableDelegate, SardesTableWidget
 """Data Input/Output plugin"""
 
 
+class DataIO(SardesPlugin):
+
+    CONF_SECTION = 'data_io'
+
+    def __init__(self, parent):
+        super().__init__(parent)
+
+    # ---- SardesPlugin Public API
+    @classmethod
+    def get_plugin_title(cls):
+        """Return widget title"""
+        return _('Data Import Wizard')
+
+    def setup_plugin(self):
+        """Setup this plugin."""
+        self.data_import_wizard = DataImportWizard(self.main)
+
+    def create_mainwindow_toolbars(self):
+        toolbar = create_mainwindow_toolbar("Data Import Wizard toolbar")
+
+        # Setup the database connection button.
+        data_import_button = create_toolbutton(
+            self.main, triggered=self._show_data_import_wizard,
+            text=_("Import Data..."),
+            tip=_("Open a wizard to import new monitoring data."),
+            icon='file_import'
+            )
+        toolbar.addWidget(data_import_button)
+
+        return [toolbar]
+
+    def close_plugin(self):
+        """
+        Extend Sardes plugin method to save user inputs in the
+        configuration file.
+        """
+        super().close_plugin()
+
+        # Save the import wizard working dir.
+        self.set_option(
+            'wiz_workdir', self.data_import_wizard.working_directory)
+
+    def register_plugin(self):
+        """
+        Extend base class method to do some connection with the database
+        manager to update the tables' data.
+        """
+        super().register_plugin()
+        self.main.db_connection_manager.register_table_model(
+            self.data_import_wizard, 'sondes_data', ['sonde_models_lib'])
+
+        # Set the import wizard working dir.
+        self.data_import_wizard.working_directory = self.get_option(
+            'wiz_workdir', None)
+
+    # ---- Private API
+    def _show_data_import_wizard(self):
+        # self._update_data_import_wizard()
+        self.data_import_wizard.show()
+
+    def _update_data_import_wizard(self):
+        self.main.db_connection_manager.update_table_model(
+            'data_import_wizard')
+
+
 class ImportDataTableModel(SardesTableModel):
     """
     A table model to display imported raw logger data.
