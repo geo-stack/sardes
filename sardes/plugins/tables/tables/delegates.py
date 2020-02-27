a0528675b606183df2b5f7b15d043e0c7844f506
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2020-02-27 11:49:19 -0500

    Add ObsWellIdEditDelegate to delegates.py module

--- sardes/plugins/tables/tables/delegates.py
+++ sardes/plugins/tables/tables/delegates.py
@@ -13,3 +13,21 @@ from qtpy.QtWidgets import QComboBox
 # ---- Local imports
 from sardes.widgets.tableviews import SardesItemDelegate
 
+
+class ObsWellIdEditDelegate(SardesItemDelegate):
+    """
+    A delegate to select an obsercation well from the list of existing well
+    in the database.
+    """
+
+    def create_editor(self, parent):
+        editor = QComboBox(parent)
+
+        # Populate the combobox with the existing observation wells.
+        obs_well_data = (self.model().libraries['observation_wells_data']
+                         .sort_values('obs_well_id', axis=0, ascending=True))
+        for index in obs_well_data.index:
+            editor.addItem(obs_well_data.loc[index, 'obs_well_id'],
+                           userData=index)
+        return editor
+
--- sardes/plugins/tables/tables/manual_measurements.py
+++ sardes/plugins/tables/tables/manual_measurements.py
@@ -7,33 +7,14 @@
 # Licensed under the terms of the GNU General Public License.
 # -----------------------------------------------------------------------------
 
-# ---- Third party imports
-from qtpy.QtWidgets import QComboBox
 
 # ---- Local imports
 from sardes.api.tablemodels import SardesTableModel
 from sardes.config.locale import _
 from sardes.widgets.tableviews import (
-    SardesItemDelegate, SardesTableWidget, TextEditDelegate,
-    NotEditableDelegate, DateTimeDelegate, NumEditDelegate)
-
-
-class ObsWellIdEditDelegate(SardesItemDelegate):
-    """
-    A delegate to select an obsercation well from the list of existing well
-    in the database.
-    """
-
-    def create_editor(self, parent):
-        editor = QComboBox(parent)
-
-        # Populate the combobox with the existing observation wells.
-        obs_well_data = (self.model().libraries['observation_wells_data']
-                         .sort_values('obs_well_id', axis=0, ascending=True))
-        for index in obs_well_data.index:
-            editor.addItem(obs_well_data.loc[index, 'obs_well_id'],
-                           userData=index)
-        return editor
+    SardesTableWidget, TextEditDelegate, NotEditableDelegate, DateTimeDelegate,
+    NumEditDelegate)
+from sardes.plugins.tables.tables.delegates import ObsWellIdEditDelegate
 
 
 class ManualMeasurementsTableModel(SardesTableModel):
