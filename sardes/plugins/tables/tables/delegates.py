06e3d1c5c38226c974875ed04baf54eb5288c626
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2020-02-27 11:48:37 -0500

    Create new delegates.py module

---
+++ sardes/plugins/tables/tables/delegates.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# Copyright © SARDES Project Contributors
+# https://github.com/cgq-qgc/sardes
+#
+# This file is part of SARDES.
+# Licensed under the terms of the GNU General Public License.
+# -----------------------------------------------------------------------------
+
+# ---- Third party imports
+from qtpy.QtWidgets import QComboBox
+
+# ---- Local imports
+from sardes.widgets.tableviews import SardesItemDelegate
+
