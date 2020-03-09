b5e0ae5a02acec0aee25c86ecf9ab439580b2d74
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2020-03-09 10:45:43 -0400

    Update database manager

--- sardes/database/database_manager.py
+++ sardes/database/database_manager.py
@@ -140,9 +140,10 @@ class DatabaseConnectionWorker(QObject):
         Get the time data acquired in the observation well for each
         monitored property listed in monitored_properties.
         """
-        prop_enum = (' and '.join(monitored_properties) if
-                     len(monitored_properties) == 2 else
-                     ', '.join(monitored_properties))
+        prop_names = [prop.name for prop in monitored_properties]
+        prop_enum = (' and '.join(prop_names) if
+                     len(prop_names) == 2 else
+                     ', '.join(prop_names))
         print("Fetching {} data for observation well {}.".format(
             prop_enum, obs_well_id))
 
