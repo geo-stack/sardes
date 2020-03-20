b9d1b8127bb74bcfc75c8d1d305cf9a3b973a301
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2020-03-20 15:14:30 -0400

    Fix tests for the import data wizard

--- sardes/plugins/dataio/tests/test_data_import _wizard.py
+++ sardes/plugins/dataio/tests/test_data_import _wizard.py
@@ -21,8 +21,8 @@ from qtpy.QtCore import Qt
 # ---- Local imports
 from sardes.database.accessor_demo import DatabaseAccessorDemo
 from sardes.database.database_manager import DatabaseConnectionManager
-from sardes.plugins.dataio.plugin import (QFileDialog, DataImportWizard,
-                                          NOT_FOUND_MSG_COLORED)
+from sardes.plugins.dataio.widgets.dataimportwizard import (
+    QFileDialog, DataImportWizard, NOT_FOUND_MSG_COLORED)
 
 
 # =============================================================================
@@ -46,6 +46,7 @@ def testdir():
 @pytest.fixture
 def data_import_wizard(qtbot, dbconnmanager):
     data_import_wizard = DataImportWizard()
+    qtbot.addWidget(data_import_wizard)
 
     dbconnmanager.register_model(
         data_import_wizard,
@@ -71,32 +72,35 @@ def test_data_import_wizard_init(qtbot, mocker, testdir, data_import_wizard):
                         return_value=(filenames.copy(), ['.csv', '.lev']))
     data_import_wizard.show()
 
+    # The first selected file is read automatically.
     assert data_import_wizard._queued_filenames == filenames[1:]
     assert data_import_wizard.working_directory == osp.dirname(filenames[-1])
+    assert data_import_wizard.table_widget.tableview.row_count() == 100
 
-    # The first selected file is read automatically.
-    assert (data_import_wizard.filename_label.text() ==
-            "solinst_level_testfile.csv")
-    assert data_import_wizard._sonde_serial_no == "1016042"
+    # Assert file infos.
+    assert data_import_wizard.filename_label.text() == filenames[0]
     assert data_import_wizard.serial_number_label.text() == "1016042"
-    assert data_import_wizard.obs_well_label.text() == '0343128'
-    assert data_import_wizard.location_label.text() == 'Municipality 1'
-    assert data_import_wizard.model_label.text() == 'Solinst LT M10 Gold'
-    assert data_import_wizard.visit_date.text() == '2016-11-26 21:45:00'
-    assert data_import_wizard.table_widget.tableview.row_count() == 300
+    assert data_import_wizard.projectid_label.text() == "03037041"
+    assert (data_import_wizard.site_name_label.text() ==
+            "SAINT-PAUL-D'ABBOTSFORD")
+
+    # Assert sonde installation infos.
+    assert (data_import_wizard.sonde_label.text() ==
+            'Solinst LT M10 Gold 1016042')
+    assert (data_import_wizard.obs_well_label.text() ==
+            "03037041 (Saint-Paul-d'Abbotsford)")
+    assert data_import_wizard.install_depth.text() == '9.02 m'
+    assert (data_import_wizard.install_period.text() ==
+            '2006-08-24 18:00 to ...')
 
     # Read the next selected file.
     qtbot.mouseClick(data_import_wizard.next_btn, Qt.LeftButton)
-    assert (data_import_wizard.filename_label.text() ==
-            "solinst_level_testfile.lev")
     assert data_import_wizard._queued_filenames == []
     assert data_import_wizard.working_directory == osp.dirname(filenames[-1])
-    assert data_import_wizard.serial_number_label.text() == "1016042"
-    assert data_import_wizard.obs_well_label.text() == '0343128'
-    assert data_import_wizard.location_label.text() == 'Municipality 1'
-    assert data_import_wizard.model_label.text() == 'Solinst LT M10 Gold'
-    assert data_import_wizard.visit_date.text() == '2017-05-01 15:37:54'
-    assert data_import_wizard.table_widget.tableview.row_count() == 300
+    assert data_import_wizard.table_widget.tableview.row_count() == 100
+
+    # Assert file infos.
+    assert data_import_wizard.filename_label.text() == filenames[1]
 
 
 if __name__ == "__main__":
