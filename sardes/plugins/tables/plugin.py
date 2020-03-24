a3db24709c455ed09da99c1c0994e10ab1d68172
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2020-03-24 15:00:38 -0400

    Optimize save edits logic

--- sardes/plugins/tables/plugin.py
+++ sardes/plugins/tables/plugin.py
@@ -97,21 +97,15 @@ class DataTableModel(SardesTableModel):
                 indexes = (date_time, obs_id, edit.column)
                 tseries_edits.loc[indexes, 'value'] = edit.edited_value
             elif edit.type() == SardesTableModel.RowDeleted:
-                for row in edit.row:
-                    row_data = self._datat.get(row)
-                    date_time = row_data['datetime']
-                    obs_id = row_data['obs_id']
-                    indexes = (date_time, obs_id, edit.column)
-                    data_types = [
-                        dt for dt in DataType if dt in row_data.keys()]
-                    for data_type in data_types:
-                        indexes = (date_time, obs_id, data_type)
-                        if indexes in tseries_edits.index:
-                            tseries_edits.drop(indexes, inplace=True)
-                        tseries_dels = tseries_dels.append(
-                            {'obs_id': obs_id,
-                             'datetime': date_time,
-                             'data_type': data_type}, ignore_index=True)
+                delrows_data = self._datat.get(edit.row)
+                data_types = [dtype for dtype in DataType if
+                              dtype in delrows_data.keys()]
+                for data_type in data_types:
+                    delrows_data_type = delrows_data[['obs_id', 'datetime']]
+                    delrows_data_type['data_type'] = data_type
+                    tseries_dels = tseries_dels.append(
+                        delrows_data_type, ignore_index=True)
+        tseries_dels.drop_duplicates()
         self.db_connection_manager.delete_timeseries_data(
             tseries_dels,
             postpone_exec=True)
