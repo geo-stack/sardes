b841ebc46efa1ce2b5b264b91c77a83cc7a354d9
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2020-03-26 12:14:13 -0400

    Add test_load_data

--- sardes/plugins/dataio/tests/test_data_import _wizard.py
+++ sardes/plugins/dataio/tests/test_data_import _wizard.py
@@ -12,17 +12,21 @@ Tests for the DataImportWizard.
 """
 
 # ---- Standard imports
+import os
 import os.path as osp
+from shutil import copyfile
 
 # ---- Third party imports
 import pytest
 from qtpy.QtCore import Qt
 
 # ---- Local imports
+from sardes.api.timeseries import DataType
 from sardes.database.accessor_demo import DatabaseAccessorDemo
 from sardes.database.database_manager import DatabaseConnectionManager
 from sardes.plugins.dataio.widgets.dataimportwizard import (
-    QFileDialog, DataImportWizard, NOT_FOUND_MSG_COLORED)
+    QFileDialog, DataImportWizard, NOT_FOUND_MSG_COLORED, QMessageBox,
+    replace_atomic)
 
 
 # =============================================================================
@@ -38,13 +42,17 @@ def dbconnmanager(qtbot):
     return dbconnmanager
 
 
-@pytest.fixture(scope="module")
-def testdir():
-    return osp.join(osp.dirname(__file__))
+@pytest.fixture
+def testfiles(tmp_path):
+    filenames = ["solinst_level_testfile.csv", "solinst_level_testfile.lev"]
+    for filename in filenames:
+        copyfile(osp.join(osp.dirname(__file__), filename),
+                 osp.join(tmp_path, filename))
+    return [osp.join(tmp_path, filename) for filename in filenames]
 
 
 @pytest.fixture
-def data_import_wizard(qtbot, dbconnmanager):
+def data_import_wizard(qtbot, dbconnmanager, testfiles, mocker):
     data_import_wizard = DataImportWizard()
     qtbot.addWidget(data_import_wizard)
 
@@ -55,30 +63,47 @@ def data_import_wizard(qtbot, dbconnmanager):
     with qtbot.waitSignal(data_import_wizard.sig_data_updated, timeout=3000):
         dbconnmanager.update_model('data_import_wizard')
 
+    exts = [osp.splitext(file)[0] for file in testfiles]
+    mocker.patch.object(
+        QFileDialog, 'getOpenFileNames', return_value=(testfiles.copy(), exts))
+    data_import_wizard.show()
+    qtbot.waitForWindowShown(data_import_wizard)
+
     return data_import_wizard
+
+
+# =============================================================================
+# ---- Utilities
+# =============================================================================
+def assert_tseries_len(data_import_wizard, data_type, expected_length):
+    """
+    Fetch the tseries data from the database for the given observation
+    well id and data type and assert that the legnth of the data is as
+    expected.
+    """
+    tseries_data = (
+        data_import_wizard.db_connection_manager.get_timeseries_for_obs_well(
+            data_import_wizard._obs_well_uuid, [data_type],
+            main_thread=True)
+        )[0].get_merged_timeseries()
+    assert len(tseries_data) == expected_length
 
 
 # =============================================================================
 # ---- Tests
 # =============================================================================
-def test_data_import_wizard_init(qtbot, mocker, testdir, data_import_wizard):
+def test_data_import_wizard_init(qtbot, mocker, testfiles, data_import_wizard):
     """
     Test that the data import wizard imports and displays data
     as expected.
     """
-    filenames = [osp.join(testdir, "solinst_level_testfile.csv"),
-                 osp.join(testdir, "solinst_level_testfile.lev")]
-    mocker.patch.object(QFileDialog, 'getOpenFileNames',
-                        return_value=(filenames.copy(), ['.csv', '.lev']))
-    data_import_wizard.show()
-
     # The first selected file is read automatically.
-    assert data_import_wizard._queued_filenames == filenames[1:]
-    assert data_import_wizard.working_directory == osp.dirname(filenames[-1])
+    assert data_import_wizard._queued_filenames == testfiles[1:]
+    assert data_import_wizard.working_directory == osp.dirname(testfiles[-1])
     assert data_import_wizard.table_widget.tableview.row_count() == 100
 
     # Assert file infos.
-    assert data_import_wizard.filename_label.text() == filenames[0]
+    assert data_import_wizard.filename_label.text() == testfiles[0]
     assert data_import_wizard.serial_number_label.text() == "1016042"
     assert data_import_wizard.projectid_label.text() == "03037041"
     assert (data_import_wizard.site_name_label.text() ==
@@ -93,15 +118,55 @@ def test_data_import_wizard_init(qtbot, mocker, testdir, data_import_wizard):
     assert (data_import_wizard.install_period.text() ==
             '2006-08-24 18:00 to ...')
 
+    # Assert internal variables values.
+    data_import_wizard._sonde_serial_no = '1016042'
+    data_import_wizard._obs_well_uuid = 0
+    data_import_wizard._sonde_depth = 9.02
+    data_import_wizard._install_id = 0
+
     # Read the next selected file.
     qtbot.mouseClick(data_import_wizard.next_btn, Qt.LeftButton)
     assert data_import_wizard._queued_filenames == []
-    assert data_import_wizard.working_directory == osp.dirname(filenames[-1])
+    assert data_import_wizard.working_directory == osp.dirname(testfiles[-1])
     assert data_import_wizard.table_widget.tableview.row_count() == 100
 
     # Assert file infos.
-    assert data_import_wizard.filename_label.text() == filenames[1]
+    assert data_import_wizard.filename_label.text() == testfiles[1]
 
+
+def test_load_data(qtbot, mocker, testfiles, data_import_wizard):
+    """
+    Test that loading new timeseries data to the database is working as
+    expected.
+    """
+    filename = testfiles[0]
+    assert_tseries_len(data_import_wizard, DataType.WaterLevel, 1826)
+    assert_tseries_len(data_import_wizard, DataType.WaterTemp, 1826)
+
+    # We first try to load the data while setting an invalid directory for the
+    # option to move the input data file after loading.
+    data_import_wizard.pathbox_widget.checkbox.setChecked(True)
+    data_import_wizard.pathbox_widget.set_path('some_non_valid_path')
+    assert data_import_wizard.pathbox_widget.is_enabled()
+    assert not data_import_wizard.pathbox_widget.is_valid()
+
+    patcher_msgbox_warning = mocker.patch.object(
+        QMessageBox, 'warning', return_value=QMessageBox.Ok)
+    qtbot.mouseClick(data_import_wizard.load_btn, Qt.LeftButton)
+    assert patcher_msgbox_warning.call_count == 1
+    assert_tseries_len(data_import_wizard, DataType.WaterLevel, 1826)
+    assert_tseries_len(data_import_wizard, DataType.WaterTemp, 1826)
+
+    # We now disbaled the option to move the input data file after loading and
+    # try to load the data again.
+    data_import_wizard.pathbox_widget.checkbox.setChecked(False)
+    with qtbot.waitSignal(data_import_wizard.table_model.sig_data_saved):
+        qtbot.mouseClick(data_import_wizard.load_btn, Qt.LeftButton)
+    assert patcher_msgbox_warning.call_count == 1
+    assert_tseries_len(data_import_wizard, DataType.WaterLevel, 1826 + 100)
+    assert_tseries_len(data_import_wizard, DataType.WaterTemp, 1826 + 100)
+    assert osp.exists(filename)
+    qtbot.wait(300)
 
 if __name__ == "__main__":
     pytest.main(['-x', osp.basename(__file__), '-v', '-rw'])
