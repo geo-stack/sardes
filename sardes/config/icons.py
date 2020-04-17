fe7d8801c4da351c1513aef2b3d7bb604a6e9f1c
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2020-04-17 11:05:59 -0400

    Change warning icon to use triangle and YELLOW color

--- sardes/config/icons.py
+++ sardes/config/icons.py
@@ -19,7 +19,7 @@ import qtawesome as qta
 
 # ---- Local imports
 from sardes import __rootdir__
-from sardes.config.gui import ICON_COLOR, GREEN, RED
+from sardes.config.gui import ICON_COLOR, GREEN, RED, YELLOW
 
 DIRNAME = os.path.join(__rootdir__, 'ressources', 'icons')
 LOCAL_ICONS = {
@@ -206,8 +206,8 @@ FA_ICONS = {
         {'color': ICON_COLOR, 'scale_factor': 1.1,
          'hflip': True}],
     'warning': [
-        ('mdi.alert-circle-outline',),
-        {'color': RED, 'scale_factor': 1.3}],
+        ('mdi.alert-outline',),
+        {'color': YELLOW, 'scale_factor': 1.3}],
     'zoom_to_rect': [
         ('mdi.magnify-plus-cursor',),
         {'color': ICON_COLOR, 'scale_factor': 1.3}],
