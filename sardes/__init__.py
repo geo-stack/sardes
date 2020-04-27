d12fd6f6aac8920f3e998866ebd72811d1f512a2
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2020-04-27 10:18:41 -0400

    Bump version to 0.8.0.dev0

--- sardes/__init__.py
+++ sardes/__init__.py
@@ -25,7 +25,7 @@ import os
 import sys
 
 
-version_info = (0, 7, 4)
+version_info = (0, 8, 0, 'dev0')
 __version__ = '.'.join(map(str, version_info))
 __appname__ = 'Sardes'
 __namever__ = __appname__ + " " + __version__
