18131d0e97126558978d31222040f6370d412d64
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2020-05-04 16:17:45 -0400

    Move TimeSeriesGroup and TimeSeries with the plot viewer

--- sardes/widgets/timeseries.py
+++ sardes/widgets/timeseries.py
@@ -9,6 +9,7 @@
 
 # ---- Standard library imports
 import sys
+from collections.abc import Mapping
 
 # ---- Third party imports
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
@@ -17,22 +18,317 @@ from matplotlib.figure import Figure as MplFigure
 from matplotlib.axes import Axes as MplAxes
 from matplotlib.widgets import RectangleSelector, SpanSelector
 from matplotlib.dates import num2date
-from qtpy.QtCore import Qt, Slot
-from qtpy.QtWidgets import QAction, QApplication, QMainWindow
+import numpy as np
+from qtpy.QtCore import Qt, Slot, QSize
+from qtpy.QtWidgets import (QAction, QApplication, QMainWindow, QLabel,
+                            QDoubleSpinBox, QWidget, QHBoxLayout)
+import pandas as pd
 from pandas.plotting import register_matplotlib_converters
 
 # ---- Local imports
+from sardes.api.timeseries import DataType
 from sardes.config.locale import _
 from sardes.config.icons import get_icon
 from sardes.config.gui import get_iconsize
 from sardes.utils.qthelpers import (
-    center_widget_to_another, create_mainwindow_toolbar, create_toolbutton)
+    center_widget_to_another, create_mainwindow_toolbar, create_toolbutton,
+    format_tooltip)
 from sardes.widgets.buttons import DropdownToolButton, SemiExclusiveButtonGroup
 
 
 register_matplotlib_converters()
 
 
+# ---- Data containers
+class TimeSeriesGroup(Mapping):
+    """
+    Sardes time series group class.
+
+    The :class:`TimeSeriesGroup` class provides a container to manage sardes
+    :class:`TimeSeries` that belongs to the same data type.
+
+    Parameters
+    ----------
+    data_type: DataType
+        The type of data constituing the time series that are contained in
+        this group.
+    prop_name: str
+        The common human readable name describing the data constituing
+        the time series that are contained in this group.
+    prop_units: str
+        The units in which the data are saved.
+    yaxis_inverted: bool
+        A boolean to indicate whether the data should be plotted on an
+        inverted y-axis (positive towards bottom).
+    """
+
+    def __init__(self, data_type, yaxis_inverted=False):
+        self._timeseries = []
+        self.data_type = DataType(data_type)
+        self.yaxis_inverted = yaxis_inverted
+
+    def __len__(self, key):
+        return len(self._tseries)
+
+    def __setitem__(self, key, value):
+        return NotImplementedError
+
+    def __getitem__(self, key):
+        return NotImplementedError
+
+    def __iter__(self):
+        for tseries in self._timeseries:
+            yield tseries
+
+    def __str__(self):
+        return self.get_merged_timeseries().__str__()
+
+    # ---- Timeseries
+    @property
+    def timeseries(self):
+        """
+        Return a list of timeseries associated with this monitored property.
+        """
+        return self._timeseries
+
+    def add_timeseries(self, tseries):
+        """
+        Add a new timeseries to this monitored property.
+        """
+        self._timeseries.append(tseries)
+
+    # ---- Utilities
+    def get_merged_timeseries(self):
+        """
+        Return a pandas dataframe containing the data from all the timeseries
+        that were added to this group.
+        """
+        if len(self.timeseries) >= 1:
+            merged_tseries = self.timeseries[0]._data.to_frame()
+            merged_tseries.columns = [self.data_type]
+            # Add series ID to the dataframe.
+            merged_tseries['obs_id'] = self.timeseries[0].id
+            # Add sonde ID to the dataframe.
+            merged_tseries['sonde_id'] = self.timeseries[0].sonde_id
+            # Add datetime to the dataframe.
+            merged_tseries['datetime'] = merged_tseries.index
+            # Reset index, but preserve the datetime data.
+            merged_tseries.reset_index(drop=True, inplace=True)
+
+            # Append or merge the remaining timeseries with the first one.
+            for tseries in self.timeseries[1:]:
+                tseries_to_append = tseries._data.to_frame()
+                tseries_to_append.columns = [self.data_type]
+                tseries_to_append['obs_id'] = tseries.id
+                tseries_to_append['sonde_id'] = tseries.sonde_id
+                tseries_to_append['datetime'] = tseries_to_append.index
+                tseries_to_append.reset_index(drop=True, inplace=True)
+                merged_tseries = merged_tseries.append(
+                    tseries_to_append, ignore_index=True,
+                    verify_integrity=True, sort=True)
+        elif len(self.timeseries) == 0:
+            merged_tseries = pd.DataFrame([])
+        return merged_tseries
+
+    # ---- Data selection
+    def clear_selected_data(self):
+        """
+        Clear all selected data in the timeseries of this timeseries group.
+        """
+        for tseries in self._timeseries:
+            tseries.clear_selected_data()
+
+    def select_data(self, *args, **kargs):
+        """
+        This is a convenience method to select data in the timeseries of this
+        group for a given period and range of values.
+        """
+        for tseries in self._timeseries:
+            tseries.select_data(*args, **kargs)
+
+
+class TimeSeries(Mapping):
+    """
+    Sardes time series class.
+
+    Attributes
+    ----------
+    data
+        A pandas Series with datetime indexes.
+    tseries_id
+        A unique ID used to reference this time series between Sardes GUI and
+        the database by the database accessor.
+    tseries_name: str
+        A common human readable name used to reference this time series in the
+        GUI and the graphs.
+    tseries_units: str
+        The units of the data this timeseries is referencing to.
+    sonde_id
+        An ID used to reference the sonde with which the data of this time
+        series were acquired.
+    """
+
+    def __init__(self, data, tseries_id, tseries_name=None,
+                 tseries_units=None, tseries_color=None,
+                 sonde_id=None):
+        super().__init__()
+        self._data = data
+        self.name = tseries_name
+        self.id = tseries_id
+        self.units = tseries_units
+        self.color = tseries_color
+        self.sonde_id = sonde_id
+
+        self._undo_stack = []
+        self._selected_data_indexes = pd.DatetimeIndex([])
+
+    def __len__(self, key):
+        return len(self._data)
+
+    def __setitem__(self, key, value):
+        return NotImplementedError
+
+    def __getitem__(self, key):
+        return NotImplementedError
+
+    def __iter__(self):
+        return NotImplementedError
+
+    def __str__(self):
+        return self._data.__str__()
+
+    # ---- Attributes
+    @property
+    def data(self):
+        return self._data
+
+    @property
+    def dates(self):
+        return self._data.index.values
+
+    @property
+    def strftime(self):
+        return self._data.index.strftime("%Y-%m-%dT%H:%M:%S").values.tolist()
+
+    # ---- Data Selection
+    def select_data(self, xrange=None, yrange=None):
+        """
+        Select data for a given period and range of values.
+
+        Return a pandas DatetimeIndex containing the datetime indexes
+        of the timeseries corresponding to the data in the specified
+        period and range of values.
+
+        The resulting datetime indexes are also added to a list of
+        already selected indexes, whose corresponding data can be obtained
+        with the get_selected_data method.
+
+        Parameters
+        ----------
+        xrange: tuple of datetime
+            A tuple of 2-datetime objects specifying the start and end of
+            a period.
+        yrange: tuple of float
+            A tuple of 2-floats specifying a range of values.
+
+        Returns
+        -------
+        pandas.DatetimeIndex
+            A pandas datetime index corresponding to the data in the
+            specified period and range of values.
+        """
+        if xrange is not None and self._data.index.tzinfo is None:
+            # Make sure the datetime objects or the specified period
+            # use the same timezone info as that of the timeseries.
+            xrange = (xrange[0].replace(tzinfo=self._data.index.tzinfo),
+                      xrange[1].replace(tzinfo=self._data.index.tzinfo))
+
+        if xrange and yrange:
+            indexes = (
+                self._data[(self._data.index >= xrange[0]) &
+                           (self._data.index <= xrange[1]) &
+                           (self._data >= yrange[0]) &
+                           (self._data <= yrange[1])
+                           ]).index
+        elif xrange:
+            indexes = (
+                self._data[(self._data.index >= xrange[0]) &
+                           (self._data.index <= xrange[1])
+                           ]).index
+        elif yrange:
+            indexes = (
+                self._data[(self._data >= yrange[0]) &
+                           (self._data <= yrange[1])
+                           ]).index
+        else:
+            indexes = pd.DatetimeIndex([])
+
+        self._selected_data_indexes = (
+            self._selected_data_indexes.append(indexes))
+
+        return indexes
+
+    def get_selected_data(self):
+        """
+        Get the previously selected data of this timeseries.
+
+        Return a pandas Series containing the data of this timeseries that
+        were previously selected by the user.
+        """
+        return self._data.loc[self._selected_data_indexes]
+
+    def clear_selected_data(self):
+        """
+        Clear the previously selected data of this timeseries.
+
+        Clear the data of this timeseries that were previously selected
+        by the user.
+        """
+        self._selected_data_indexes = pd.DatetimeIndex([])
+
+    # ---- Versionning
+    @property
+    def has_uncommited_changes(self):
+        """"
+        Return whether there is uncommited changes to the water level data.
+        """
+        return bool(len(self._undo_stack))
+
+    def commit(self):
+        """Commit the changes made to the water level data to the project."""
+        raise NotImplementedError
+
+    def undo(self):
+        """Undo the last changes made to the water level data."""
+        if self.has_uncommited_changes:
+            changes = self._undo_stack.pop(-1)
+            self._data[changes.index] = changes
+
+    def clear_all_changes(self):
+        """
+        Clear all changes that were made to the water level data since the
+        last commit.
+        """
+        while self.has_uncommited_changes:
+            self.undo()
+
+    def delete_waterlevels_at(self, indexes):
+        """Delete the water level data at the specified indexes."""
+        if len(indexes):
+            self._add_to_undo_stack(indexes)
+            self._data.iloc[indexes] = np.nan
+
+    def _add_to_undo_stack(self, indexes):
+        """
+        Store the old water level values at the specified indexes in a stack
+        before changing or deleting them. This allow to undo or cancel any
+        changes made to the water level data before commiting them.
+        """
+        if len(indexes):
+            self._undo_stack.append(self._data.iloc[indexes, 0].copy())
+
+
+# ---- Plotting devices
 class TimeSeriesAxes(MplAxes):
     """
     A matplotlib Axes object where one or more timeseries of the same
