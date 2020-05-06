749c03de57bc4fc08d906c4a55d8fec82da99ca6
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2020-05-06 15:27:18 -0400

    Add test_sonde_data_interface
    Test that the interface for adding, editing and deleting sondes from the
    database is working as expected.

--- sardes/database/tests/test_accessor_sardes_lite.py
+++ sardes/database/tests/test_accessor_sardes_lite.py
@@ -12,6 +12,7 @@ Tests for the DatabaseAccessorSardesLite.
 """
 
 # ---- Standard imports
+import datetime
 import os.path as osp
 import uuid
 
@@ -27,8 +28,9 @@ from sardes.database.accessor_sardes_lite import (
 # =============================================================================
 # ---- Fixtures
 # =============================================================================
-@pytest.fixture
-def dbaccessor(tmp_path):
+@pytest.fixture(scope="module")
+def dbaccessor(tmp_path_factory):
+    tmp_path = tmp_path_factory.mktemp("database")
     dbaccessor = DatabaseAccessorSardesLite(
         osp.join(tmp_path, 'sqlite_database_test.db'))
     init_database(dbaccessor)
@@ -38,7 +40,7 @@ def dbaccessor(tmp_path):
 # =============================================================================
 # ---- Tests
 # =============================================================================
-def test_observation_well_interface(dbaccessor):
+def test_observation_wells_interface(dbaccessor):
     """
     Test that the interface for adding, editing and deleting observation
     wells from the database is working as expected.
@@ -49,7 +51,7 @@ def test_observation_well_interface(dbaccessor):
     assert len(obs_wells) == 0
 
     # Add a new observation well to the database.
-    sampling_feature_uuid = uuid.uuid4()
+    sampling_feature_uuid = dbaccessor._create_index('observation_wells_data')
     attribute_values = {
         'obs_well_id': '123A2456',
         'latitude': 48.0775,
@@ -98,6 +100,63 @@ def test_observation_well_interface(dbaccessor):
         assert (obs_wells.at[sampling_feature_uuid, attribute_name] ==
                 attribute_value), attribute_name
 
+
+def test_sonde_data_interface(dbaccessor):
+    """
+    Test that the interface for adding, editing and deleting sondes
+    from the database is working as expected.
+    """
+    sonde_models = dbaccessor.get_sonde_models_lib()
+    assert len(sonde_models) > 0
+
+    sonde_data = dbaccessor.get_sondes_data()
+    assert len(sonde_data) == 0
+
+    # Add a new sonde to the database.
+    sonde_feature_uuid = dbaccessor._create_index('sondes_data')
+    attribute_values = {
+        'sonde_serial_no': '1015973',
+        'sonde_model_id': sonde_models.index[0],
+        'date_reception': datetime.date(2006, 3, 30),
+        'date_withdrawal': pd.NaT,
+        'in_repair': False,
+        'out_of_order': False,
+        'lost': False,
+        'off_network': False,
+        'sonde_notes': 'This is a fake sonde for testing purposes only.'
+        }
+    dbaccessor.add_sondes_data(sonde_feature_uuid, attribute_values)
+
+    sonde_data = dbaccessor.get_sondes_data()
+    assert len(sonde_data) == 1
+    for attribute_name, attribute_value in attribute_values.items():
+        if attribute_name == 'date_withdrawal':
+            assert pd.isnull(sonde_data.at[sonde_feature_uuid, attribute_name])
+        else:
+            assert (sonde_data.at[sonde_feature_uuid, attribute_name] ==
+                    attribute_value), attribute_name
+
+    # Edit the data of the newly added sonde.
+    edited_attribute_values = {
+        'sonde_serial_no': '1015973',
+        'sonde_model_id': sonde_models.index[1],
+        'date_reception': datetime.date(2006, 3, 30),
+        'date_withdrawal': datetime.date(2010, 6, 12),
+        'in_repair': True,
+        'out_of_order': True,
+        'lost': True,
+        'off_network': True,
+        'sonde_notes': 'Edited fake sonde note.'
+        }
+    for attribute_name, attribute_value in edited_attribute_values.items():
+        dbaccessor.set_sondes_data(
+            sonde_feature_uuid, attribute_name, attribute_value)
+
+    sonde_data = dbaccessor.get_sondes_data()
+    assert len(sonde_data) == 1
+    for attribute_name, attribute_value in edited_attribute_values.items():
+        assert (sonde_data.at[sonde_feature_uuid, attribute_name] ==
+                attribute_value), attribute_name
 
 if __name__ == "__main__":
     pytest.main(['-x', osp.basename(__file__), '-v', '-rw'])
