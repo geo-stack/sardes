5aebd667156257431bda907f4ba02532bc96df29
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2020-05-14 08:14:47 -0400

    Don't set current axe if empty

--- sardes/widgets/timeseries.py
+++ sardes/widgets/timeseries.py
@@ -1154,7 +1154,8 @@ class TimeSeriesPlotViewer(QMainWindow):
         self.axes_toolbar.setEnabled(self.current_axe_button.count())
 
         # We want the water level axe to be the active one by default.
-        self.set_current_axe(0)
+        if self.current_axe_button.count():
+            self.set_current_axe(0)
 
     def update_data(self, dataf, obs_well_data=None):
         """Set the data that need to be displayed in this plot viewer."""
