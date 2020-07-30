6e854febb394b7a474ee6295d5f74947fd61a124
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2020-07-30 17:36:36 -0400

    Implement ImportFromClipboardTool for the manual measurements table

--- sardes/plugins/tables/tables/manual_measurements.py
+++ sardes/plugins/tables/tables/manual_measurements.py
@@ -7,6 +7,8 @@
 # Licensed under the terms of the GNU General Public License.
 # -----------------------------------------------------------------------------
 
+# ---- Thid party imports
+import pandas as pd
 
 # ---- Local imports
 from sardes.api.tablemodels import SardesTableModel
@@ -15,6 +17,125 @@ from sardes.widgets.tableviews import (
     SardesTableWidget, TextEditDelegate, NotEditableDelegate, DateTimeDelegate,
     NumEditDelegate)
 from sardes.plugins.tables.tables.delegates import ObsWellIdEditDelegate
+from sardes.api.tools import SardesTool
+
+
+class ImportFromClipboardTool(SardesTool):
+    """
+    A tool to append the content on the clipboard to the manual measurements
+    table.
+    """
+
+    def __init__(self, parent):
+        super().__init__(
+            parent,
+            name='import_from_clipboard',
+            text=_('Import from Clipboard'),
+            icon='import_clipboard',
+            tip=_('Add content on the Clipboard to this table.')
+            )
+
+    def __triggered__(self):
+        new_data = pd.read_clipboard(sep='\t', dtype='str', header=None)
+
+        table_visible_columns = self.parent.tableview.visible_columns()
+
+        data_columns_mapper = self.parent.model()._data_columns_mapper
+        table_visible_labels = [
+            data_columns_mapper[column].lower().replace(' ', '')
+            for column in table_visible_columns]
+
+        new_data_columns = []
+        for i in range(len(new_data.columns)):
+            new_data_i = new_data.iat[0, i].lower().replace(' ', '')
+            if new_data_i in table_visible_columns:
+                new_data_columns.append(new_data_i)
+            elif new_data_i in table_visible_labels:
+                index = table_visible_labels.index(new_data_i)
+                new_data_columns.append(table_visible_columns[index])
+            else:
+                new_data_columns = (
+                    table_visible_columns[:len(new_data.columns)])
+                break
+        else:
+            # This means that the headers were correctly provided in
+            # the copied data. We then need to drop the first row of the data.
+            new_data.drop(new_data.index[0], axis='index', inplace=True)
+        new_data.columns = new_data_columns
+
+        warning_messages = []
+        for column in new_data.columns:
+            if column in 'datetime':
+                try:
+                    new_data['datetime'] = pd.to_datetime(
+                        new_data['datetime'], format="%Y-%m-%d %H:%M")
+                except ValueError:
+                    warning_messages.append(_(
+                        "Some date time data did not match the "
+                        "yyyy-mm-dd hh:mm specified format"))
+                    new_data['datetime'] = pd.to_datetime(
+                        new_data['datetime'],
+                        format="%Y-%m-%d %H:%M",
+                        errors='coerce')
+            elif column == 'value':
+                try:
+                    new_data['value'] = pd.to_numeric(new_data['value'])
+                except ValueError:
+                    warning_messages.append(_(
+                        "Some water level manual measurement data could not "
+                        "be converted to numerical value"))
+                    new_data['value'] = pd.to_numeric(
+                        new_data['value'], errors='coerce')
+            elif column == 'sampling_feature_uuid':
+                isnull1 = new_data['sampling_feature_uuid'].isnull()
+                try:
+                    obs_wells = (
+                        self.parent.model().libraries['observation_wells_data']
+                        )
+                    obs_wells_dict = (
+                        obs_wells['obs_well_id']
+                        [obs_wells['obs_well_id'].isin(
+                            new_data['sampling_feature_uuid'])]
+                        .drop_duplicates()
+                        .reset_index()
+                        .set_index('obs_well_id')
+                        .to_dict()['sampling_feature_uuid']
+                        )
+                    new_data['sampling_feature_uuid'] = (
+                        new_data['sampling_feature_uuid']
+                        .map(obs_wells_dict.get)
+                        )
+                except KeyError:
+                    pass
+                else:
+                    isnull2 = new_data['sampling_feature_uuid'].isnull()
+                    if sum(isnull1 != isnull2):
+                        warning_messages.append(_(
+                            "Some well ID data did not match any well ID "
+                            "in the database"))
+        values = new_data.to_dict(orient='records')
+
+        if values != [{'sampling_feature_uuid': None}]:
+            self.parent.tableview._append_row(values)
+            if len(warning_messages):
+                formatted_message = _(
+                    "The following error(s) occurred while adding the "
+                    "content on the Clipboard to this table:")
+                formatted_message += (
+                    '<ul style="margin-left:-30px"><li>{}.</li></ul>'.format(
+                        ';</li><li>'.join(warning_messages)))
+                self.parent.show_message(
+                    title=_("Import from Clipboard warning"),
+                    message=formatted_message,
+                    func='warning')
+        else:
+            # This means that the Clipboard doesn't contain any valid data
+            # to add to this table.
+            self.parent.show_message(
+                title=_("Import from Clipboard warning"),
+                message=_("The Clipboard does not contain any valid data "
+                          "to add to this table."),
+                func='warning')
 
 
 class ManualMeasurementsTableModel(SardesTableModel):
@@ -70,3 +191,7 @@ class ManualMeasurementsTableWidget(SardesTableWidget):
                 ('notes', _('Notes'))]
             )
         super().__init__(table_model, *args, **kargs)
+
+        # Add the tool to import data from the clipboard.
+        self.install_tool(
+            ImportFromClipboardTool(self), after='copy_to_clipboard')
