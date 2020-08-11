6a57028814e475b2e157bd63cbbf95c1993c5817
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2020-08-11 10:20:31 -0400

    Move ImportFromClipboardTool to tableview module

--- sardes/widgets/tableviews.py
+++ sardes/widgets/tableviews.py
@@ -11,7 +11,6 @@
 # ---- Standard imports
 import sys
 from datetime import datetime
-import itertools
 from math import floor, ceil
 
 # ---- Third party imports
@@ -31,6 +30,7 @@ from qtpy.QtWidgets import (
 from sardes import __appname__
 from sardes.api.panes import SardesPaneWidget
 from sardes.api.tablemodels import SardesSortFilterModel, SardesTableModelBase
+from sardes.api.tools import SardesTool
 from sardes.config.icons import get_icon
 from sardes.config.locale import _
 from sardes.config.gui import get_iconsize
@@ -471,6 +471,110 @@ class BoolEditDelegate(SardesItemDelegate):
         editor.addItem(_('Yes'), userData=True)
         editor.addItem(_('No'), userData=False)
         return editor
+
+
+# =============================================================================
+# ---- Tools
+# =============================================================================
+class ImportFromClipboardTool(SardesTool):
+    """
+    A tool to append the Clipboard contents to a Sardes table widget.
+    """
+
+    def __init__(self, parent):
+        super().__init__(
+            parent,
+            name='import_from_clipboard',
+            text=_('Import from Clipboard'),
+            icon='import_clipboard',
+            tip=_('Add the Clipboard contents to this table.')
+            )
+
+    def __triggered__(self):
+        new_data = pd.read_clipboard(sep='\t', dtype='str', header=None)
+        if new_data.empty:
+            self.parent.show_message(
+                title=_("Import from Clipboard warning"),
+                message=_("Nothing was added to the table because the "
+                          "Clipboard was empty."),
+                func='warning')
+            return
+
+        table_visible_columns = self.parent.tableview.visible_columns()
+        if len(new_data.columns) > len(table_visible_columns):
+            self.parent.show_message(
+                title=_("Import from Clipboard warning"),
+                message=_("The Clipboard contents cannot be added to "
+                          "the table because the number of columns of the "
+                          "copied data is too large."),
+                func='warning')
+            return
+
+        data_columns_mapper = self.parent.model()._data_columns_mapper
+        table_visible_labels = [
+            data_columns_mapper[column].lower().replace(' ', '')
+            for column in table_visible_columns]
+
+        new_data_columns = []
+        for i in range(len(new_data.columns)):
+            new_data_i = (
+                '' if pd.isnull(new_data.iat[0, i]) else new_data.iat[0, i]
+                ).lower().replace(' ', '')
+            if new_data_i in table_visible_columns:
+                new_data_columns.append(new_data_i)
+            elif new_data_i in table_visible_labels:
+                index = table_visible_labels.index(new_data_i)
+                new_data_columns.append(table_visible_columns[index])
+            else:
+                break
+        if len(new_data.columns) == len(set(new_data_columns)):
+            # This means that the headers were correctly provided in
+            # the copied data. We then need to drop the first row of the data.
+            new_data.drop(new_data.index[0], axis='index', inplace=True)
+        else:
+            # This means that there was a problem reading one or more column
+            # names, that  or
+            # that the columns names were not provided with the imported data.
+            new_data_columns = table_visible_columns[:len(new_data.columns)]
+        new_data.columns = new_data_columns
+
+        warning_messages = []
+        for column in new_data.columns:
+            delegate = self.parent.tableview.itemDelegateForColumn(
+                self.parent.model().columns.index(column))
+            new_data[column], warning_message = delegate.format_data(
+                new_data[column])
+            if warning_message is not None:
+                warning_messages.append(warning_message)
+
+        formatted_message = None
+        if new_data.isnull().values.flatten().all():
+            formatted_message = _(
+                "Nothing was added to the table because the Clipboard "
+                "did not contain any valid data.")
+            if new_data.size > 1 and len(warning_messages):
+                formatted_message += "<br><br>"
+                formatted_message += _(
+                    "The following error(s) occurred while trying to add the "
+                    "Clipboard contents to this table:")
+                formatted_message += (
+                    '<ul style="margin-left:-30px"><li>{}.</li></ul>'.format(
+                        ';</li><li>'.join(warning_messages)))
+        else:
+            values = new_data.to_dict(orient='records')
+            self.parent.tableview._append_row(values)
+            if len(warning_messages):
+                formatted_message = _(
+                    "The following error(s) occurred while adding the "
+                    "Clipboard contents to this table:")
+                formatted_message += (
+                    '<ul style="margin-left:-30px"><li>{}.</li></ul>'.format(
+                        ';</li><li>'.join(warning_messages)))
+        if formatted_message is not None:
+            self.parent.show_message(
+                title=_("Import from Clipboard warning"),
+                message=formatted_message,
+                func='warning')
 
 
 # =============================================================================
