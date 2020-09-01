0fc78aa971660136fa63314c1b9dc1d649486e3b
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2020-09-01 13:26:57 -0400

    Updates tests for the database connection worker

--- sardes/database/tests/test_database_manager.py
+++ sardes/database/tests/test_database_manager.py
@@ -12,13 +12,11 @@ Tests for the DatabaseConnectionManager class.
 """
 
 # ---- Standard imports
-import os.path as osp
 from time import sleep
 
 # ---- Third party imports
 import pytest
 import pandas as pd
-from flaky import flaky
 
 # ---- Local imports
 from sardes.database.database_manager import DatabaseConnectionManager
@@ -36,7 +34,9 @@ def DATAF():
 @pytest.fixture
 def dbmanager(qtbot, mocker):
     dbmanager = DatabaseConnectionManager()
-    return dbmanager
+    yield dbmanager
+
+    qtbot.waitUntil(lambda: not dbmanager._thread.isRunning())
 
 
 @pytest.fixture
@@ -58,12 +58,7 @@ def dbaccessor(qtbot, mocker, DATAF):
 # =============================================================================
 # ---- Tests
 # =============================================================================
-def test_dbmanager_init(dbmanager):
-    """Test that the databse connection manager is initialized correctly."""
-    assert dbmanager
-
-
-def test_dbmanager_connect(dbmanager, dbaccessor, qtbot):
+def test_connect_to_database(dbmanager, dbaccessor, qtbot):
     """
     Test that the database connection manager connection to the database is
     working as expected.
@@ -89,14 +84,10 @@ def test_dbmanager_connect(dbmanager, dbaccessor, qtbot):
     assert not dbmanager.is_connected()
     assert len(dbmanager._running_tasks) == 0
 
-    qtbot.waitUntil(lambda: not dbmanager._db_connection_thread.isRunning())
 
-
-@flaky(max_runs=3)
-def test_run_tasks_if_posponed(dbmanager, dbaccessor, qtbot):
+def test_get_and_set_in_database(dbmanager, dbaccessor, qtbot):
     """
-    Test that the database manager is managing the queued as expected
-    when the execution is postponed.
+    Test that the database manager is managing get and set tasks as expected.
     """
     with qtbot.waitSignal(dbmanager.sig_database_connection_changed,
                           timeout=1500):
@@ -132,70 +123,6 @@ def test_run_tasks_if_posponed(dbmanager, dbaccessor, qtbot):
     assert returned_values[1]['values'].values.tolist() == [1, 2, 3, 4]
     assert returned_values[2]['values'].values.tolist() == [1, 2, -19.5, 4]
 
-    qtbot.waitUntil(lambda: not dbmanager._db_connection_thread.isRunning())
-
-
-def test_run_tasks_if_busy(dbmanager, dbaccessor, qtbot):
-    """
-    Test that the database manager is managing the queued as expected
-    when adding new tasks while the worker is busy.
-    """
-    with qtbot.waitSignal(dbmanager.sig_database_connection_changed,
-                          timeout=1500):
-        dbmanager.connect_to_db(dbaccessor)
-
-    returned_values = []
-
-    def task_callback(dataf):
-        returned_values.append(dataf)
-
-    with qtbot.waitSignal(dbmanager.sig_run_tasks_finished, timeout=5000):
-        # We send the task to the manager, but we ask to pospone the execution
-        # of each task so that they can be executed by the worker all at once.
-        dbmanager.get('something', callback=task_callback, postpone_exec=True)
-        dbmanager.get('something', callback=task_callback, postpone_exec=True)
-        dbmanager.set('something', 2, -19.5, postpone_exec=True)
-        assert len(dbmanager._queued_tasks) == 3
-        assert len(dbmanager._pending_tasks) == 0
-        assert len(dbmanager._running_tasks) == 0
-
-        # Then we ask the manager to start executing the tasks.
-        dbmanager.run_tasks()
-        assert len(dbmanager._queued_tasks) == 0
-        assert len(dbmanager._pending_tasks) == 0
-        assert len(dbmanager._running_tasks) == 3
-        assert dbmanager._db_connection_thread.isRunning()
-
-        # While the worker is running, we send another task, but postpone its
-        # execution.
-        dbmanager.set('something', 1, 0.512, postpone_exec=True)
-        assert len(dbmanager._queued_tasks) == 1
-        assert len(dbmanager._pending_tasks) == 0
-        assert len(dbmanager._running_tasks) == 3
-        assert dbmanager._db_connection_thread.isRunning()
-
-        # While the worker is still running, we send another task, but do not
-        # postpone its execution. This should cause this task and the previous
-        # one to be moved as pending tasks.
-
-        # Once the first stack of tasks is executed, the additional 2
-        # other tasks should be executed automatically.
-        dbmanager.get('something', callback=task_callback, postpone_exec=False)
-        assert len(dbmanager._queued_tasks) == 0
-        assert len(dbmanager._pending_tasks) == 2
-        assert len(dbmanager._running_tasks) == 3
-        assert dbmanager._db_connection_thread.isRunning()
-
-    # We then assert that all tasks have been executed as expected.
-    assert len(dbmanager._queued_tasks) == 0
-    assert len(dbmanager._pending_tasks) == 0
-    assert len(dbmanager._running_tasks) == 0
-
-    assert len(returned_values) == 3
-    assert returned_values[0]['values'].values.tolist() == [1, 2, 3, 4]
-    assert returned_values[1]['values'].values.tolist() == [1, 2, 3, 4]
-    assert returned_values[2]['values'].values.tolist() == [1, 0.512, -19.5, 4]
-
 
 if __name__ == "__main__":
-    pytest.main(['-x', osp.basename(__file__), '-v', '-rw'])
+    pytest.main(['-x', __file__, '-v', '-rw'])
