96114755ad48a0fc7fc19b9a34869b5c135fef2e
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2020-10-09 10:04:12 -0400

    Setup an __init__ file in tools

---
+++ sardes/plugins/readings/tools/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# Copyright © SARDES Project Contributors
+# https://github.com/cgq-qgc/sardes
+#
+# This file is part of SARDES.
+# Licensed under the terms of the GNU General Public License.
+# -----------------------------------------------------------------------------
+
+from .save2excel import SaveReadingsToExcelTool
+from .hydrogrammes import HydrographTool
