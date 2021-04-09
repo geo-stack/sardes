d86f44cb1b63e6314cb2b8d039bcb2e3da22ae7d
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2021-04-09 15:11:06 -0400

    Increase version_info and __date__ in __init__.py

--- sardes/__init__.py
+++ sardes/__init__.py
@@ -25,11 +25,11 @@ import os
 import sys
 
 
-version_info = (0, 11, 1)
+version_info = (0, 11, 2)
 __version__ = '.'.join(map(str, version_info))
 __appname__ = 'Sardes'
 __namever__ = __appname__ + " " + __version__
-__date__ = '09/03/2021'
+__date__ = '09/04/2021'
 __project_url__ = "https://github.com/cgq-qgc/sardes"
 __releases_url__ = __project_url__ + "/releases"
 __releases_api__ = "https://api.github.com/repos/cgq-qgc/sardes/releases"
