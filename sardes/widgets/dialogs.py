9d7189048d11a0ff10d23f5ae0639a53c4fc09ee
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2021-04-29 09:39:43 -0400

    ExceptDialog: rework dialog text

--- sardes/widgets/dialogs.py
+++ sardes/widgets/dialogs.py
@@ -81,9 +81,10 @@ class ExceptDialog(QDialog):
             <p>We are sorry, but {1} encountered an internal error that might
             preventing it from running correctly. You might want to save
             your work and restart {1} if possible.</p>
-            <p>Please report this error on our <a href="{2}">issues tracker</a>
-            by copying the information below and by providing a detailed
-            step-by-step description of what led to the problem.</p>
+            <p>Please report this error by copying the information below
+            in our <a href="{2}">issues tracker</a> and by providing
+            a step-by-step description of what led to the problem.
+            </p>
             """).format(__namever__, __appname__, __issues_url__))
         msg_labl.setWordWrap(True)
         msg_labl.setOpenExternalLinks(True)
