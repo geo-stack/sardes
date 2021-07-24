a6f51df03e5c1a57bf5446969ef230865344dfdc
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2021-07-24 17:42:45 -0400

    Add test for Sonde Installations table

---
+++ sardes/plugins/tables/tables/tests/test_table_sonde_installations.py
@@ -0,0 +1,168 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# Copyright © SARDES Project Contributors
+# https://github.com/cgq-qgc/sardes
+#
+# This file is part of SARDES.
+# Licensed under the terms of the GNU General Public License.
+# -----------------------------------------------------------------------------
+
+"""
+Tests for the Sonde Installations table.
+"""
+
+# ---- Standard imports
+from datetime import datetime, date
+import os
+import os.path as osp
+os.environ['SARDES_PYTEST'] = 'True'
+
+# ---- Third party imports
+import pandas as pd
+import pytest
+
+# ---- Local imports
+from sardes.widgets.tableviews import MSEC_MIN_PROGRESS_DISPLAY
+
+
+# =============================================================================
+# ---- Fixtures
+# =============================================================================
+@pytest.fixture
+def tablewidget(mainwindow, qtbot, dbaccessor, sondes_installation):
+    # Select the tab corresponding to the observation wells table.
+    tablewidget = mainwindow.plugin._tables['table_sonde_installations']
+    mainwindow.plugin.tabwidget.setCurrentWidget(tablewidget)
+    tableview = tablewidget.tableview
+    qtbot.waitUntil(
+        lambda: tableview.visible_row_count() == len(sondes_installation))
+    qtbot.wait(MSEC_MIN_PROGRESS_DISPLAY + 100)
+
+    yield tablewidget
+
+    # We need to wait for the mainwindow to close properly to avoid
+    # runtime errors on the c++ side.
+    with qtbot.waitSignal(mainwindow.sig_about_to_close):
+        mainwindow.close()
+
+
+# =============================================================================
+# ---- Tests
+# =============================================================================
+def test_add_sonde_installations(tablewidget, qtbot, sondes_installation,
+                                 dbaccessor):
+    """
+    Test that adding new sonde installations is working as expected.
+    """
+    tableview = tablewidget.tableview
+
+    # We add a new row and assert that the UI state is as expected.
+    new_row = len(sondes_installation)
+    assert tableview.visible_row_count() == len(sondes_installation)
+    tableview.new_row_action.trigger()
+    assert tableview.visible_row_count() == len(sondes_installation) + 1
+    assert tableview.model().is_new_row_at(tableview.current_index())
+    assert tableview.get_data_for_row(new_row) == [''] * 6
+
+    # Save the changes to the database.
+    saved_values = dbaccessor.get_sonde_installations()
+    assert len(saved_values) == len(sondes_installation)
+
+    with qtbot.waitSignal(tableview.model().sig_data_updated):
+        tableview._save_data_edits(force=True)
+
+    saved_values = dbaccessor.get_sonde_installations()
+    assert len(saved_values) == len(sondes_installation) + 1
+
+
+def test_edit_sonde_installations(tablewidget, qtbot, sondes_installation,
+                                  obswells_data, sondes_data, dbaccessor):
+    """
+    Test that editing sonde installations is working as expected.
+    """
+    tableview = tablewidget.tableview
+
+    edited_values = {
+        'sampling_feature_uuid': obswells_data.index[3],
+        'sonde_uuid': sondes_data.index[3],
+        'start_date': datetime(2010, 8, 1, 12, 0),
+        'end_date': datetime(2010, 8, 30, 19, 16),
+        'install_depth': 6.34,
+        'install_note': 'Edited sonde install note.'
+        }
+
+    # Edit each editable field of the first row of the table.
+    assert tableview.get_data_for_row(0) == [
+        '03037041', 'Solinst Barologger M1.5 - 1016042',
+        '2006-08-24 18:00', '2020-12-31 07:14', '9.02',
+        'Note for first sonde installation.']
+    for col in range(tableview.visible_column_count()):
+        current_index = tableview.set_current_index(0, col)
+        if not tableview.is_data_editable_at(current_index):
+            continue
+
+        orig_value = tableview.model().get_value_at(current_index)
+        edit_value = edited_values[tableview.visible_columns()[col]]
+        assert orig_value != edit_value
+
+        assert not tableview.model().is_data_edited_at(current_index)
+        tableview.edit(current_index)
+        item_delegate = tableview.itemDelegate(tableview.current_index())
+        item_delegate.set_editor_data(edit_value)
+        item_delegate.commit_data()
+        assert tableview.model().is_data_edited_at(current_index)
+        assert tableview.model().get_value_at(current_index) == edit_value
+    assert tableview.get_data_for_row(0) == [
+        '03040002', 'Solinst LT M10 Gold - 1016387',
+        '2010-08-01 12:00', '2010-08-30 19:16', '6.34',
+        'Edited sonde install note.']
+
+    # Save the changes to the database.
+    with qtbot.waitSignal(tableview.model().sig_data_updated):
+        tableview._save_data_edits(force=True)
+
+    saved_values = dbaccessor.get_sonde_installations().iloc[0].to_dict()
+    for key in edited_values.keys():
+        assert saved_values[key] == edited_values[key], key
+
+
+def test_clear_sonde_installations(tablewidget, qtbot, dbaccessor):
+    """
+    Test that clearing sonde installations is working as expected.
+    """
+    tableview = tablewidget.tableview
+    clearable_attrs = ['end_date', 'install_note']
+
+    # Clear each non required field of the first row of the table.
+    assert tableview.get_data_for_row(0) == [
+        '03037041', 'Solinst Barologger M1.5 - 1016042',
+        '2006-08-24 18:00', '2020-12-31 07:14', '9.02',
+        'Note for first sonde installation.']
+    for col in range(tableview.visible_column_count()):
+        current_index = tableview.set_current_index(0, col)
+        column = tableview.visible_columns()[col]
+        if tableview.is_data_required_at(current_index):
+            assert column not in clearable_attrs, column
+        else:
+            assert column in clearable_attrs, column
+
+            assert not tableview.model().is_data_edited_at(current_index)
+            assert not tableview.model().is_null(current_index)
+            tableview.clear_item_action.trigger()
+            assert tableview.model().is_data_edited_at(current_index)
+            assert tableview.model().is_null(current_index)
+    assert tableview.get_data_for_row(0) == [
+        '03037041', 'Solinst Barologger M1.5 - 1016042',
+        '2006-08-24 18:00', '', '9.02', '']
+
+    # Save the changes to the database.
+    with qtbot.waitSignal(tableview.model().sig_data_updated):
+        tableview._save_data_edits(force=True)
+
+    saved_values = dbaccessor.get_sonde_installations().iloc[0].to_dict()
+    for attr in clearable_attrs:
+        assert pd.isnull(saved_values[attr])
+
+
+if __name__ == "__main__":
+    pytest.main(['-x', __file__, '-v', '-rw'])
