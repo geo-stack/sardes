53ec67bc02ab53120e544de3164422b6e417eb74
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2021-09-24 16:50:36 -0400

    Setup a conftest for the "tables" module tests

---
+++ sardes/tables/tests/conftest.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# Copyright © SARDES Project Contributors
+# https://github.com/cgq-qgc/sardes
+#
+# This file is part of SARDES.
+# Licensed under the terms of the GNU General Public License.
+# -----------------------------------------------------------------------------
+
+# ---- Standard imports
+import os
+import os.path as osp
+os.environ['SARDES_PYTEST'] = 'True'
+
+# ---- Third party imports
+import pytest
+
+# ---- Local imports
+from sardes.database.database_manager import DatabaseConnectionManager
+from sardes.tables.managers import SardesTableModelsManager
+from sardes.database.accessors import DatabaseAccessorSardesLite
+from sardes.database.accessors.tests.conftest import *
+
+
+@pytest.fixture
+def dbaccessor(tmp_path, database_filler):
+    dbaccessor = DatabaseAccessorSardesLite(
+        osp.join(tmp_path, 'sqlite_database_test.db'))
+    dbaccessor.init_database()
+    database_filler(dbaccessor)
+
+    return dbaccessor
+
+
+@pytest.fixture
+def dbconnmanager(dbaccessor, qtbot):
+    dbconnmanager = DatabaseConnectionManager()
+    with qtbot.waitSignal(dbconnmanager.sig_database_connected, timeout=3000):
+        dbconnmanager.connect_to_db(dbaccessor)
+    assert dbconnmanager.is_connected()
+    return dbconnmanager
+
+
+@pytest.fixture
+def tablesmanager(dbconnmanager):
+    tablesmanager = SardesTableModelsManager(dbconnmanager)
+    return tablesmanager
