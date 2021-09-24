0acc966e20b3cedefbca2a8b7835b3ff2bcddf97
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2021-09-24 17:02:56 -0400

    Move tests for the table "Sondes Inventory" to the "tables" module

--- sardes/tables/tests/test_table_sondes_inventory.py
+++ sardes/tables/tests/test_table_sondes_inventory.py
@@ -12,9 +12,8 @@ Tests for the Sondes Inventory table.
 """
 
 # ---- Standard imports
-from datetime import datetime, date
+from datetime import date
 import os
-import os.path as osp
 os.environ['SARDES_PYTEST'] = 'True'
 
 # ---- Third party imports
@@ -22,27 +21,30 @@ import pandas as pd
 import pytest
 
 # ---- Local imports
-from sardes.widgets.tableviews import MSEC_MIN_PROGRESS_DISPLAY
+from sardes.tables import SondesInventoryTableWidget
 
 
 # =============================================================================
 # ---- Fixtures
 # =============================================================================
 @pytest.fixture
-def tablewidget(mainwindow, qtbot, dbaccessor, sondes_data):
-    # Select the tab corresponding to the observation wells table.
-    tablewidget = mainwindow.plugin._tables['table_sondes_inventory']
-    mainwindow.plugin.tabwidget.setCurrentWidget(tablewidget)
-    qtbot.waitUntil(
-        lambda: tablewidget.tableview.visible_row_count() == len(sondes_data))
-    qtbot.wait(MSEC_MIN_PROGRESS_DISPLAY + 100)
+def tablewidget(tablesmanager, qtbot, dbaccessor, sondes_data):
+    tablewidget = SondesInventoryTableWidget()
+    qtbot.addWidget(tablewidget)
+    tablewidget.show()
 
-    yield tablewidget
+    tablemodel = tablewidget.model()
+    tablesmanager.register_table_model(tablemodel)
 
-    # We need to wait for the mainwindow to close properly to avoid
-    # runtime errors on the c++ side.
-    with qtbot.waitSignal(mainwindow.sig_about_to_close):
-        mainwindow.close()
+    # This connection is usually made by the plugin, but we need to make it
+    # here manually for testing purposes.
+    tablesmanager.sig_models_data_changed.connect(tablemodel.update_data)
+
+    with qtbot.waitSignal(tablemodel.sig_data_updated):
+        tablemodel.update_data()
+    assert tablewidget.tableview.visible_row_count() == len(sondes_data)
+
+    return tablewidget
 
 
 # =============================================================================
