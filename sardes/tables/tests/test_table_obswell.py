a4bc3497e1943eeb4d8d1db8f8283646a73b61fa
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2021-12-23 10:22:36 -0500

    Mark 'test_delete_observation_well' as flaky
    See https://github.com/cgq-qgc/sardes/issues/528

--- sardes/tables/tests/test_table_obswell.py
+++ sardes/tables/tests/test_table_obswell.py
@@ -16,6 +16,7 @@ import os
 os.environ['SARDES_PYTEST'] = 'True'
 
 # ---- Third party imports
+from flaky import flaky
 import numpy as np
 import pandas as pd
 import pytest
@@ -336,6 +337,7 @@ def test_clear_observation_well(tablewidget, qtbot, dbaccessor, obswells_data):
         assert pd.isnull(saved_values[attr])
 
 
+@flaky(max_runs=3)
 def test_delete_observation_well(tablewidget, qtbot, dbaccessor, mocker,
                                  dbconnmanager):
     """
