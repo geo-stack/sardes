a6b6ea8def0d5573b466a48dd917149fab86636d
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2023-02-13 16:11:38 -0500

    Expand tests for the database connection widget

--- sardes/plugins/databases/tests/test_database_connection.py
+++ sardes/plugins/databases/tests/test_database_connection.py
@@ -28,6 +28,8 @@ from sardes.plugins.databases.widgets import DatabaseConnectionWidget
 from sardes.widgets.statusbar import ProcessStatusBar
 from sardes.api.database_dialog import DatabaseConnectDialogBase
 from sardes.api.database_accessor import DatabaseAccessorBase
+from sardes.database.accessors.accessor_errors import (
+    DatabaseVersionError, DatabaseUpdateError)
 
 
 # =============================================================================
@@ -36,6 +38,15 @@ from sardes.api.database_accessor import DatabaseAccessorBase
 class DatabaseAccessorMock(DatabaseAccessorBase):
     dbconnection = None
     dbconnection_error = None
+
+    def version(self):
+        return 2
+
+    def req_version(self):
+        return 3
+
+    def update_database(self):
+        pass
 
     def _connect(self):
         pass
@@ -74,6 +85,16 @@ def test_dbconnwidget_init(dbconnwidget):
     assert dbconnwidget
     assert dbconnwidget.status_bar.status == ProcessStatusBar.HIDDEN
 
+    assert dbconnwidget.connect_button.isEnabled()
+    assert dbconnwidget.close_button.isEnabled()
+    assert dbconnwidget.cancel_button.isEnabled()
+    assert dbconnwidget.update_button.isEnabled()
+
+    assert dbconnwidget.connect_button.isVisible()
+    assert dbconnwidget.close_button.isVisible()
+    assert not dbconnwidget.cancel_button.isVisible()
+    assert not dbconnwidget.update_button.isVisible()
+
 
 def test_dbconnwidget_connect(dbconnwidget, qtbot, mocker):
     """
@@ -99,16 +120,22 @@ def test_dbconnwidget_connect(dbconnwidget, qtbot, mocker):
         assert dbconnmanager.is_connecting()
         assert dbconnwidget.status_bar.status == ProcessStatusBar.IN_PROGRESS
         assert not dbconnwidget.stacked_dialogs.isEnabled()
+        assert not dbconnwidget.dbtype_combobox.isEnabled()
         assert not dbconnwidget.connect_button.isEnabled()
         assert not dbconnwidget.close_button.isEnabled()
+        assert not dbconnwidget.cancel_button.isVisible()
+        assert not dbconnwidget.update_button.isVisible()
 
     # Assert that a connection to the database was created sucessfully.
     assert dbconnmanager.is_connected() is True
     assert dbconnwidget.status_bar.status == ProcessStatusBar.PROCESS_SUCCEEDED
     assert not dbconnwidget.stacked_dialogs.isEnabled()
+    assert not dbconnwidget.dbtype_combobox.isEnabled()
     assert dbconnwidget.connect_button.isEnabled()
     assert dbconnwidget.connect_button.text() == 'Disconnect'
     assert dbconnwidget.close_button.isEnabled()
+    assert not dbconnwidget.cancel_button.isVisible()
+    assert not dbconnwidget.update_button.isVisible()
     assert not dbconnwidget.isVisible()
 
     dbconnwidget.show()
@@ -120,9 +147,12 @@ def test_dbconnwidget_connect(dbconnwidget, qtbot, mocker):
     assert dbconnmanager.is_connected() is False
     assert dbconnwidget.status_bar.status == ProcessStatusBar.HIDDEN
     assert dbconnwidget.stacked_dialogs.isEnabled()
+    assert dbconnwidget.dbtype_combobox.isEnabled()
     assert dbconnwidget.connect_button.isEnabled()
     assert dbconnwidget.connect_button.text() == 'Connect'
     assert dbconnwidget.close_button.isEnabled()
+    assert not dbconnwidget.cancel_button.isVisible()
+    assert not dbconnwidget.update_button.isVisible()
 
 
 @pytest.mark.parametrize('mode', ['return none', 'raise exception'])
@@ -134,37 +164,145 @@ def test_dbconnwidget_failed_connect(mode, dbconnwidget, qtbot, mocker):
     dbconnmanager = dbconnwidget.db_connection_manager
 
     def _connect_mock(*args, **kargs):
-        qtbot.wait(300)
         if mode == 'return none':
             connection = None
             connection_error = None
         elif mode == 'raise exception':
             connection = None
-            connection_error = OperationalError(Mock(), Mock(), Mock())
+            connection_error = OperationalError(
+                Mock(), Mock(), Mock())
         return connection, connection_error
 
     mocker.patch.object(
         DatabaseAccessorMock, '_connect', side_effect=_connect_mock)
 
     # Try connecting to the database.
-    with qtbot.waitSignal(dbconnmanager.sig_database_connected,
-                          timeout=3000):
+    with qtbot.waitSignal(dbconnmanager.sig_database_connected):
         qtbot.mouseClick(dbconnwidget.connect_button, Qt.LeftButton)
-
-        assert dbconnmanager.is_connecting()
-        assert dbconnwidget.status_bar.status == ProcessStatusBar.IN_PROGRESS
-        assert not dbconnwidget.stacked_dialogs.isEnabled()
-        assert not dbconnwidget.connect_button.isEnabled()
-        assert not dbconnwidget.close_button.isEnabled()
 
     # Assert that the connection to the database failed.
     assert dbconnmanager.is_connected() is False
     assert dbconnwidget.status_bar.status == ProcessStatusBar.PROCESS_FAILED
     assert dbconnwidget.stacked_dialogs.isEnabled()
+    assert dbconnwidget.dbtype_combobox.isEnabled()
     assert dbconnwidget.connect_button.isEnabled()
     assert dbconnwidget.close_button.isEnabled()
     assert dbconnwidget.isVisible()
 
 
+def test_dbconnwidget_need_update(dbconnwidget, qtbot, mocker):
+    """
+    Test the database connection manager when the connection to the database
+    fails because the database schema is outdated.
+    """
+    dbconnmanager = dbconnwidget.db_connection_manager
+
+    # Try connecting to an outdated database.
+    mocker.patch.object(
+        DatabaseAccessorMock, '_connect',
+        return_value=(None, DatabaseVersionError(cur_version=2, req_version=3))
+        )
+
+    with qtbot.waitSignal(dbconnmanager.sig_database_connected):
+        qtbot.mouseClick(dbconnwidget.connect_button, Qt.LeftButton)
+
+    # Assert that the connection to the database failed and is showing an
+    # option to update the database.
+    assert dbconnmanager.is_connected() is False
+    assert dbconnwidget.status_bar.status == ProcessStatusBar.NEED_UPDATE
+
+    assert not dbconnwidget.stacked_dialogs.isEnabled()
+    assert not dbconnwidget.dbtype_combobox.isEnabled()
+
+    assert not dbconnwidget.connect_button.isVisible()
+    assert not dbconnwidget.close_button.isVisible()
+    assert dbconnwidget.cancel_button.isVisible()
+    assert dbconnwidget.update_button.isVisible()
+    assert dbconnwidget.isVisible()
+
+    # Update the database unsuccessfully.
+    def _update_mock(*args, **kargs):
+        from_version = 2
+        to_version = 3
+        error = DatabaseUpdateError(
+            from_version,
+            to_version,
+            ValueError('Mocked update database error.'))
+        return from_version, to_version, error
+
+    mocker.patch.object(
+        DatabaseAccessorMock, 'update_database',
+        side_effect=_update_mock
+        )
+
+    with qtbot.waitSignal(dbconnmanager.sig_database_updated):
+        qtbot.mouseClick(dbconnwidget.update_button, Qt.LeftButton)
+
+    assert dbconnmanager.is_connected() is False
+    assert dbconnwidget.status_bar.status == ProcessStatusBar.PROCESS_FAILED
+    assert dbconnwidget.stacked_dialogs.isEnabled()
+    assert dbconnwidget.dbtype_combobox.isEnabled()
+    assert dbconnwidget.connect_button.isEnabled()
+    assert dbconnwidget.close_button.isEnabled()
+    assert dbconnwidget.isVisible()
+
+    # Try to connect again.
+    with qtbot.waitSignal(dbconnmanager.sig_database_connected):
+        qtbot.mouseClick(dbconnwidget.connect_button, Qt.LeftButton)
+
+    # Assert that the connection to the database failed again and is
+    # showing again an option to update the database.
+    assert dbconnmanager.is_connected() is False
+    assert dbconnwidget.status_bar.status == ProcessStatusBar.NEED_UPDATE
+
+    assert not dbconnwidget.stacked_dialogs.isEnabled()
+    assert not dbconnwidget.dbtype_combobox.isEnabled()
+
+    assert not dbconnwidget.connect_button.isVisible()
+    assert not dbconnwidget.close_button.isVisible()
+    assert dbconnwidget.cancel_button.isVisible()
+    assert dbconnwidget.update_button.isVisible()
+    assert dbconnwidget.isVisible()
+
+    # Update the database successfully.
+    def _update_mock(*args, **kargs):
+        qtbot.wait(300)
+        from_version = 2
+        to_version = 3
+        error = None
+        return from_version, to_version, error
+
+    mocker.patch.object(
+        DatabaseAccessorMock, 'update_database',
+        side_effect=_update_mock
+        )
+
+    with qtbot.waitSignal(dbconnmanager.sig_database_updated):
+        qtbot.mouseClick(dbconnwidget.update_button, Qt.LeftButton)
+
+        assert dbconnmanager.is_updating()
+        assert dbconnwidget.status_bar.status == ProcessStatusBar.IN_PROGRESS
+        assert not dbconnwidget.stacked_dialogs.isEnabled()
+        assert not dbconnwidget.dbtype_combobox.isEnabled()
+        assert dbconnwidget.cancel_button.isVisible()
+        assert dbconnwidget.update_button.isVisible()
+        assert not dbconnwidget.cancel_button.isEnabled()
+        assert not dbconnwidget.update_button.isEnabled()
+        assert not dbconnwidget.connect_button.isVisible()
+        assert not dbconnwidget.close_button.isVisible()
+
+    # Assert that the database was updated sucessfully.
+    assert dbconnmanager.is_updating() is False
+    assert dbconnwidget.status_bar.status == ProcessStatusBar.PROCESS_SUCCEEDED
+    assert dbconnwidget.stacked_dialogs.isEnabled()
+    assert dbconnwidget.dbtype_combobox.isEnabled()
+    assert dbconnwidget.connect_button.isEnabled()
+    assert dbconnwidget.close_button.isEnabled()
+    assert dbconnwidget.connect_button.isVisible()
+    assert dbconnwidget.close_button.isVisible()
+    assert not dbconnwidget.cancel_button.isVisible()
+    assert not dbconnwidget.update_button.isVisible()
+
+
 if __name__ == "__main__":
-    pytest.main(['-x', osp.basename(__file__), '-v', '-rw'])
+    pytest.main(['-x', __file__, '-v', '-rw'])
