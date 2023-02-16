aebffb4b0afc1626ac86ebbe7aea97e25475615b
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2023-02-16 11:00:17 -0500

    Add back set_model_data extension in obs_well table
    This should be done in another PR

--- sardes/tables/table_observation_wells.py
+++ sardes/tables/table_observation_wells.py
@@ -111,6 +111,19 @@ class ObsWellsTableModel(StandardSardesTableModel):
                 return
         super()._check_foreign_constraint(callback)
 
+    def set_model_data(self, dataf, dataf_columns_mapper=None):
+        """
+        Extend SardesTableModelBase base class method to make sure we use
+        a column dtype that is capable to handle integer nan values.
+
+        See https://pandas.pydata.org/pandas-docs/stable/user_guide/
+            gotchas.html#support-for-integer-na
+        """
+        if 'aquifer_code' not in dataf.columns:
+            dataf['aquifer_code'] = None
+        dataf['aquifer_code'] = dataf['aquifer_code'].astype(pd.Int64Dtype())
+        return super().set_model_data(dataf, dataf_columns_mapper)
+
     # ---- Visual Data
     def logical_to_visual_data(self, visual_dataf):
         """
