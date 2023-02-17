1d8ce8880fa000a835f8de5033994419be3bd7d0
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2023-02-16 19:14:51 -0500

    SondesSelectionDelegate: implement logical_to_visual_data

--- sardes/tables/delegates.py
+++ sardes/tables/delegates.py
@@ -358,3 +358,29 @@ class SondesSelectionDelegate(SardesItemDelegate):
                 editor.addItem(
                     values['sonde_brand_model_serial'], userData=index)
         return editor
+
+    def logical_to_visual_data(self, visual_dataf):
+        """
+        Transform logical data to visual data.
+        """
+        try:
+            sondes_data = self.libraries['sondes_data']
+            sonde_models_lib = self.libraries['sonde_models_lib']
+
+            sondes_data['sonde_brand_model'] = sonde_models_lib.loc[
+                sondes_data['sonde_model_id']]['sonde_brand_model'].values
+
+            mask = sondes_data['sonde_serial_no'].notnull()
+            sondes_data['sonde_brand_model_serial'] = sondes_data[
+                'sonde_brand_model']
+            sondes_data.loc[mask, 'sonde_brand_model_serial'] = (
+                sondes_data['sonde_serial_no'] +
+                ' - ' +
+                sondes_data['sonde_brand_model'])
+
+            visual_dataf['sonde_uuid'] = (
+                visual_dataf['sonde_uuid']
+                .map(sondes_data['sonde_brand_model_serial'].to_dict().get)
+                )
+        except KeyError:
+            pass
