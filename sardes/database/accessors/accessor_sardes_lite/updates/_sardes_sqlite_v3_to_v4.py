27c3f77ac1fc9eea7d46b95f99a187da270155dd
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2023-03-20 21:54:48 -0400

    Create _sardes_sqlite_v3_to_v4.py

---
+++ sardes/database/accessors/accessor_sardes_lite/updates/_sardes_sqlite_v3_to_v4.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# Copyright © SARDES Project Contributors
+# https://github.com/cgq-qgc/sardes
+#
+# This file is part of SARDES.
+# Licensed under the terms of the GNU General Public License.
+# -----------------------------------------------------------------------------
+
+"""
+Scripts to update the Sardes SQLite database schema.
+"""
+from __future__ import annotations
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from sardes.database.accessors import DatabaseAccessorSardesLite
+import pandas as pd
+
+
+def _update_v3_to_v4(accessor: DatabaseAccessorSardesLite):
+    """
+    Update Sardes SQLite database schema to version 4 from version 3.
+
+    The objective is to save 'in_recharge_zone' and 'is_influenced' data as
+    integers instead of string in the database.
+    """
+    # Fetch data from the database.
+    select_statement = (
+        """
+        SELECT sampling_feature_uuid, in_recharge_zone, is_influenced
+        FROM sampling_feature_metadata;
+        """
+    )
+    data = pd.read_sql_query(
+        select_statement, accessor._session.connection(),
+        index_col='sampling_feature_uuid'
+        )
+
+    # Delete and create new columns with the right type.
+    accessor.execute(
+        "ALTER TABLE sampling_feature_metadata DROP in_recharge_zone;"
+    )
+    accessor.execute(
+        "ALTER TABLE sampling_feature_metadata ADD in_recharge_zone INTEGER;"
+    )
+    accessor.execute(
+        "ALTER TABLE sampling_feature_metadata DROP is_influenced;"
+    )
+    accessor.execute(
+        "ALTER TABLE sampling_feature_metadata ADD is_influenced INTEGER;"
+    )
+
+    # Transform 'in_recharge_zone' and 'is_influenced' data from str to Int64.
+    int_map_dict = {'Oui': 1, 'oui': 1, 'Non': 0, 'non': 0, 'ND': 3, 'nd': 3}
+    data['in_recharge_zone'] = (
+        data['in_recharge_zone']
+        .map(int_map_dict.get)
+        .astype('Int64')
+        )
+    data['is_influenced'] = (
+        data['is_influenced']
+        .map(int_map_dict.get)
+        .astype('Int64')
+        )
+
+    # Save the transformed data back in the database.
+    for index, row in data.iterrows():
+        accessor.execute(
+            """
+            UPDATE sampling_feature_metadata
+            SET in_recharge_zone = :is_rechg, is_influenced = :is_inf
+            WHERE sampling_feature_uuid = :uuid;
+            """,
+            {'is_rechg': row['in_recharge_zone'],
+             'is_inf': row['is_influenced'],
+             'uuid': index}
+        )
