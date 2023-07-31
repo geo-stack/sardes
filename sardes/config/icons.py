f34cf709eb35dfca70dc135f6fdc8446c1036aa9
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2023-07-31 12:19:46 -0400

    Update icons.py

--- sardes/config/icons.py
+++ sardes/config/icons.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # -----------------------------------------------------------------------------
 # Copyright © SARDES Project Contributors
-# https://github.com/cgq-qgc/sardes
+# https://github.com/geo-stack/sardes
 #
 # This file is part of SARDES.
 # Licensed under the terms of the GNU General Public License.
