804a633177615e9218e4ff51ca2a8d1125fd1128
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2023-07-31 12:19:51 -0400

    Create updates.py

---
+++ sardes/widgets/updates.py
@@ -0,0 +1,257 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# Copyright © SARDES Project Contributors
+# https://github.com/geo-stack/sardes
+#
+# This file is part of SARDES.
+# Licensed under the terms of the GNU General Public License.
+#
+# Copyright (c) 2017 Spyder Project Contributors
+# https://github.com/spyder-ide/spyder
+#
+# Some parts of this file is a derivative work of the Spyder project.
+# Licensed under the terms of the MIT License.
+# https://github.com/spyder-ide/spyder/master/spyder/workers/updates.py
+# https://github.com/spyder-ide/spyder/blob/master/spyder/utils/programs.py
+#
+# Copyright (C) 2013 The IPython Development Team
+# https://github.com/ipython/ipython
+#
+# See gwhat/__init__.py for more details.
+# -----------------------------------------------------------------------------
+
+# ---- Standard imports
+import re
+from distutils.version import LooseVersion
+
+# ---- Third party imports
+from qtpy.QtCore import QObject, Qt, QThread, Signal
+from qtpy.QtWidgets import QApplication, QMessageBox
+import requests
+
+# ---- Local imports
+from sardes import (
+    __version__, __releases_url__, __releases_api__,
+    __namever__, __project_url__)
+from sardes.config.icons import get_icon
+from sardes.config.locale import _
+
+
+class UpdatesManager(QObject):
+    """
+    Self contained manager that checks if updates are available on GitHub
+    and displays the ressults in a message box.
+    """
+
+    def __init__(self, parent=None):
+        super().__init__()
+
+        self.dialog_updates = UpdatesDialog(parent)
+        self._latest_release = None
+        self._update_available = None
+        self._show_only_if_update = False
+
+        self.thread_updates = QThread()
+
+        self.worker_updates = WorkerUpdates()
+        self.worker_updates.moveToThread(self.thread_updates)
+        self.worker_updates.sig_ready.connect(self._receive_updates_check)
+
+        self.thread_updates.started.connect(self.worker_updates.start)
+
+    def start_updates_check(self, show_only_if_update: bool = False):
+        """Check if updates are available."""
+        self._show_only_if_update = show_only_if_update
+        self.thread_updates.start()
+
+    def _receive_updates_check(self, update_available, latest_release, error):
+        """Receive results from an update check."""
+        self.thread_updates.quit()
+        if update_available is False and self._show_only_if_update is True:
+            return
+
+        if error is not None:
+            icn = QMessageBox.Warning
+            msg = error
+        else:
+            icn = QMessageBox.Information
+            if update_available:
+                msg = _(
+                    "<p><b>Sardes {} is available!</b></p>"
+                    "<p>This new version can be downloaded from our "
+                    "<a href={}>Releases</a> page.</p>"
+                    ).format(latest_release, __releases_url__)
+            else:
+                url_m = __project_url__ + "/milestones"
+                url_t = __project_url__ + "/issues"
+                msg = _(
+                    "<p><b>{} is up to date</b></p>"
+                    "<p>Further information about Sardes releases are "
+                    "available on our <a href={}>Releases</a> page.</p>"
+                    "<p>The roadmap of the Sardes project can be consulted "
+                    "on our <a href={}>Milestones</a> page.</p>"
+                    "<p>Please help Sardes by reporting bugs or proposing "
+                    "new features on our <a href={}>Issues Tracker</a>.</p>"
+                    ).format(__namever__, __releases_url__, url_m, url_t)
+
+        self.dialog_updates.setText(msg)
+        self.dialog_updates.setIcon(icn)
+        self.dialog_updates.exec_()
+
+
+class UpdatesDialog(QMessageBox):
+    """
+    Dialog to display update checks.
+    """
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.setWindowTitle(_('Updates'))
+        self.setWindowIcon(get_icon('master'))
+        self.setMinimumSize(800, 700)
+        self.setWindowFlags(
+            Qt.Window | Qt.CustomizeWindowHint | Qt.WindowCloseButtonHint)
+
+        self.setStandardButtons(QMessageBox.Ok)
+        self.setDefaultButton(QMessageBox.Ok)
+
+
+class WorkerUpdates(QObject):
+    """
+    Worker that checks for releases using the Github API.
+    """
+    sig_ready = Signal(object, object, object)
+
+    def __init__(self):
+        super(WorkerUpdates, self).__init__()
+        self.error = None
+        self.latest_release = None
+        self.update_available = False
+
+    def start(self):
+        """Main method of the WorkerUpdates worker."""
+        self.update_available = False
+        self.latest_release = __version__
+        self.error = None
+
+        try:
+            page = requests.get(__releases_api__)
+            data = page.json()
+        except requests.exceptions.HTTPError:
+            self.error = _(
+                "Unable to retrieve information because of an http error.")
+        except requests.exceptions.ConnectionError:
+            self.error = (
+                "Unable to connect to the internet. Make "
+                "sure that your connection is working properly.")
+        except requests.exceptions.Timeout:
+            self.error = (
+                "Unable to retrieve information because the "
+                "connection timed out.")
+        except Exception:
+            self.error = (
+                "Unable to check for updates because of "
+                "an unexpected error.")
+
+        releases = [item['tag_name'] for item in data]
+        result = check_update_available(__version__, releases)
+        self.update_available, self.latest_release = result
+
+        self.sig_ready.emit(
+            self.update_available, self.latest_release, self.error)
+
+
+def check_update_available(version, releases):
+    """
+    Checks if there is an update available.
+
+    It takes as parameters the current version of GWHAT and a list of
+    valid cleaned releases in chronological order (what github api returns
+    by default). Example: ['2.3.4', '2.3.3' ...]
+
+    Copyright (c) Spyder Project Contributors
+    Licensed under the terms of the MIT License
+    """
+    if is_stable_version(version):
+        # Remove non stable versions from the list.
+        releases = [r for r in releases if is_stable_version(r)]
+
+    if len(releases) == 0:
+        return False, None
+
+    latest_release = releases[0]
+    if version.endswith('dev'):
+        return (False, latest_release)
+    else:
+        return (check_version(version, latest_release, '<'),
+                latest_release)
+
+
+def check_version(actver, version, cmp_op):
+    """
+    Check version string of an active module against a required version.
+
+    If dev/prerelease tags result in TypeError for string-number comparison,
+    it is assumed that the dependency is satisfied. Users on dev branches are
+    responsible for keeping their own packages up to date.
+
+    Copyright (C) 2013 The IPython Development Team
+    Licensed under the terms of the BSD License
+    """
+    if isinstance(version, tuple):
+        version = '.'.join([str(i) for i in version])
+
+    # Hacks needed so that LooseVersion understands that (for example)
+    # version = '3.0.0' is in fact bigger than actver = '3.0.0rc1'
+    if (is_stable_version(version) and not is_stable_version(actver) and
+            actver.startswith(version) and version != actver):
+        version = version + 'zz'
+    elif (is_stable_version(actver) and not is_stable_version(version) and
+            version.startswith(actver) and version != actver):
+        actver = actver + 'zz'
+
+    try:
+        if cmp_op == '>':
+            return LooseVersion(actver) > LooseVersion(version)
+        elif cmp_op == '>=':
+            return LooseVersion(actver) >= LooseVersion(version)
+        elif cmp_op == '=':
+            return LooseVersion(actver) == LooseVersion(version)
+        elif cmp_op == '<':
+            return LooseVersion(actver) < LooseVersion(version)
+        elif cmp_op == '<=':
+            return LooseVersion(actver) <= LooseVersion(version)
+        else:
+            return False
+    except TypeError:
+        return True
+
+
+def is_stable_version(version):
+    """
+    Returns wheter this is a stable version or not. A stable version has no
+    letters in the final component, but only numbers.
+
+    Stable version example: 1.2, 1.3.4, 1.0.5
+    Not stable version: 1.2alpha, 1.3.4beta, 0.1.0rc1, 3.0.0dev
+
+    Copyright (c) 2017 Spyder Project Contributors
+    Licensed under the terms of the MIT License
+    """
+    if not isinstance(version, tuple):
+        version = version.split('.')
+    last_part = version[-1]
+
+    if not re.search('[a-zA-Z]', last_part):
+        return True
+    else:
+        return False
+
+
+if __name__ == "__main__":
+    import sys
+    app = QApplication(sys.argv)
+    updates_manager = UpdatesManager()
+    updates_manager.start_updates_check(show_only_if_update=False)
+    sys.exit(app.exec_())
